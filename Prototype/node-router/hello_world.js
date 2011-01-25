26dcde5ac5083b69f46a5555f3e00ab6ac32df7e
David Easter <david.easter@lmco.com>
2011-01-25 23:39:35 +0000

    2011-01-20, "Prototype copy 5".
    git-svn-id: http://svn.virtualworldframework.com/trunk@19 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ Prototype/node-router/hello_world.js
@@ -0,0 +1,12 @@
+var server = require('./lib/node-router').getServer();
+
+server.get("/json", function (req, res, match) {
+  return {hello: "World"};
+});
+
+server.get(new RegExp("^/(.*)$"), function hello(req, res, match) {
+  return "Hello " + (match || "World") + "!";
+});
+
+
+server.listen(8080);
