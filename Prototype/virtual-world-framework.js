4682d15e146a456ff127b2f0f4872d082dcc4fc8
David Easter <david.easter@lmco.com>
2011-01-25 23:30:45 +0000

    2011-01-19 version ("Prototype copy 4").
    git-svn-id: http://svn.virtualworldframework.com/trunk@18 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- Prototype/virtual-world-framework.js
+++ Prototype/virtual-world-framework.js
@@ -37,54 +37,142 @@ jQuery.extend( {
     VirtualWorldFramework.initialize = function( rootElementSelector ) {
 
         var worldURI = jQuery.getQueryString( "world" );
-        // alert( "initialized: world = " + worldURI );
+        var parentQuery = jQuery( rootElementSelector );
 
-        jQuery.getJSON( worldURI, function( data ) { VirtualWorldFramework.load( data, rootElementSelector ) } );
+        jQuery.getJSON( worldURI, function( data ) { VirtualWorldFramework.loadWorld( parentQuery, data ) } );
 
-	};
+        return parentQuery;
 
-    VirtualWorldFramework.load = function( data, rootElementSelector ) {
+	}; // initialize
+
+    VirtualWorldFramework.loadWorld = function( parentQuery, data ) {
 
         VirtualWorldFramework.world = data;
 
-        // alert( "loading: title = " + data.title );
+        parentQuery.addClass( "vwf-node vwf-depth-0" );
 
-console.log( "world " + VirtualWorldFramework.world.children.length );
+        VirtualWorldFramework.createProperties( parentQuery, data.properties );
+        VirtualWorldFramework.createMethods( parentQuery, data.methods );
+        VirtualWorldFramework.createEvents( parentQuery, data.events );
+        VirtualWorldFramework.createChildren( parentQuery, data.children );
         
-        jQuery.each( VirtualWorldFramework.world.children, function( index, value ) {
-            var childQuery = VirtualWorldFramework.findOrCreate( jQuery( rootElementSelector ), index );
+    }; // loadWorld
 
+    VirtualWorldFramework.createProperties = function( parentQuery, properties ) {
 
-if ( value.children )
-{
-console.log( "parent " + value.children.length );
-            jQuery.each( value.children, function( index, value ) {
-                VirtualWorldFramework.findOrCreate( childQuery, index );
+        if ( properties ) {
+
+            Object.defineProperty( properties, "values", { value: {}, enumerable: false } );
+
+            var containerQuery = VirtualWorldFramework.createBlock( parentQuery, "Properties", "vwf-properties", true );
+
+            jQuery.each( properties, function( index, value ) {
+
+                var childQuery = VirtualWorldFramework.createBlock( containerQuery, index + ": " + value, "vwf-property", true );
+
+                Object.defineProperty( properties, index, {
+                    get: function() { console.log( "get " + index ); return properties.values[index] },
+                    set: function( value ) { console.log( "set " + index ); childQuery.children().text( index + ": " + value ); properties.values[index] = value }
+                } );
+
+                properties.values[index] = value;
+
             } );
-}
-        } );
 
-    };
+        }
+        
+    }; // createProperties
+
+    VirtualWorldFramework.createMethods = function( parentQuery, methods ) {
+
+        if ( methods ) {
+
+            var containerQuery = VirtualWorldFramework.createBlock( parentQuery, "Methods", "vwf-methods", true );
+
+            jQuery.each( methods, function( index, value ) {
+                var childQuery = VirtualWorldFramework.createBlock( containerQuery, index, "vwf-method", true );
+                // record value
+            } );
+
+        }
+        
+    }; // createMethods
+
+    VirtualWorldFramework.createEvents = function( parentQuery, events ) {
+
+        if ( events ) {
+
+            var containerQuery = VirtualWorldFramework.createBlock( parentQuery, "Events", "vwf-events", true );
+
+            jQuery.each( events, function( index, value ) {
+                var childQuery = VirtualWorldFramework.createBlock( containerQuery, index, "vwf-event", true );
+                // record value
+            } );
+
+        }
+        
+    }; // createProperties
+
+    VirtualWorldFramework.createChildren = function( parentQuery, children ) {
+
+        if ( children ) {
+
+            // make container: class = vwf-children
+            // make each child: class = vwf-node, vwf-depth
+
+            var containerQuery = VirtualWorldFramework.createBlock( parentQuery, "Children", "vwf-children", true );
+
+            var parentClass = parentQuery.attr( "class" ) || "";
+            var parentDepth = Number( ( " " + parentClass + " " ).match( / vwf-depth-(\d+) / )[1] || "0" );
+
+            var childDepth = parentDepth + 1;
+            var childClass = "vwf-node vwf-depth-" + childDepth;
+
+            jQuery.each( children, function( index, value ) {
+                // var childQuery = VirtualWorldFramework.findOrCreate( parentQuery, index );
+                var childQuery = VirtualWorldFramework.createBlock( containerQuery, index, childClass, true );
+                VirtualWorldFramework.createChildren( childQuery, value.children );
+            } );
+
+        }
+
+    }; // createChildren
+
+
+    // A node, a container for properties, methods, events, or children, or property, method, or event.
     
-    VirtualWorldFramework.findOrCreate = function( parentQuery, childName ) {
+    // node container (children): id = path.from.root.children, class = vwf-children, label = "children"
+    // property container: id = path.from.root.properties, class = vwf-properties, label = "properties"
+    // method container: id = path.from.root.methods, class = vwf-properties, label = "methods"
+    // event container: id = path.from.root.events, class = vwf-properties, label = "events"
 
-        var parentClass = parentQuery.attr( "class" ) || "";
-        var parentDepth = Number( ( " " + parentClass + " " ).match( / vwf-depth-(\d+) / )[1] || "0" );
+    // node: id = path.from.root.children.name (cleaned), class = vwf-node, vwf-depth-xxx, label = name
+    // property: id = path.from.root.properties.name (cleaned), class = vwf-property, label = ?
+    // method: id = path.from.root.methods.name (cleaned), class = vwf-method, label = ?
+    // event: id = path.from.root.events.name (cleaned), class = vwf-event, label = ?
 
-        var childDepth = parentDepth + 1;
-        var childClass = "vwf-node vwf-depth-" + childDepth;
+    VirtualWorldFramework.createBlock = function( parentQuery, childName, childClass, labelChild ) {
 
-        var childQuery = parentQuery.append( "<div id='" + childName + "' class='" + childClass + "'></div>" ).children().last().
-            append( "<p>" + childName + "</p>" ).children().last().
-                // append( "<div id='" + childName + "b' class='vwf-node vwf-depth-2'></div>" ).children().last().
-                //     append( "<p>" + childName + "b</p>" ).
-                // end().end().
-            end().end();
+        var parentID = parentQuery.attr( "id" ) || "";
+        var childID = ( parentID && parentID + "." ) + childName;
+
+        var childIDHTML = childID ? "id='" + childID + "' " : "";
+        var childClassHTML = childClass ? "class='" + childClass + "' " : "";
+
+        var childQuery = parentQuery.append( "<div " + childIDHTML + childClassHTML + "></div>" ).children().last();
+        
+        if ( labelChild )
+        {
+            var childLabelHTML = childName;
+            var childLabelClassHTML = "class='vwf-label'";
+
+            childQuery.append( "<p " + childLabelClassHTML + ">" + childLabelHTML + "</p>" );
+        }
 
         return childQuery;
 
-    };
-
+    }; // createBlock
+    
     return window.VirtualWorldFramework = VirtualWorldFramework;
 
 } ) ( window );
