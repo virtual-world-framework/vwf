13dab95e9973167e20ef704f17cc5d33647320b1
David Easter <david.easter@lmco.com>
2011-01-26 00:55:32 +0000

    2011-01-24, "P2 copy 6". Simulation shard working.
    git-svn-id: http://svn.virtualworldframework.com/trunk@29 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- Prototype/client/vwf-shard-javascript.js
+++ Prototype/client/vwf-shard-javascript.js
@@ -4,8 +4,10 @@
 
         var map = {};
         
-        this.onConstruct = function( nodeID, nodeType, source, mimeType ) {
-            map[nodeID] = new Node( nodeID, source, mimeType );
+        this.onConstruct = function( nodeID, nodeType, nodeName, source, mimeType ) {
+
+            map[nodeID] = new Node( nodeID, nodeName, source, mimeType );
+
         };
 
         this.onDestruct = function( nodeID ) {
@@ -17,17 +19,14 @@
             var node = map[nodeID];
             var child = map[childID];
 
-            if ( node ) {
-                node.children[child.name] = child;
-                node.children.push( child );
+            node.children[child.name] = child;
+            node.children.push( child );
 
-                Object.defineProperty( node, child.name, {
-                    get: function() { return child },
-                    set: function( child ) { }, // TODO
-                    enumerable: true
-                } );
-            }
-
+            Object.defineProperty( node, child.name, {
+                get: function() { return child },
+                set: function( child ) { }, // TODO
+                enumerable: true
+            } );
         };
 
         this.onChildRemoved = function( nodeID ) {
@@ -44,14 +43,35 @@
 
         this.onCreateProperty = function( nodeID, propertyName, propertyValue ) {
 
+            var node = map[nodeID];
+            var property = node.properties[propertyName] = new Property( node, propertyValue ); // TODO: value from json
+
+            Object.defineProperty( node, propertyName, {
+                get: function() { return vwf.getProperty( nodeID, propertyName ) },
+                set: function( value ) { vwf.setProperty( nodeID, propertyName, value ) },
+                enumerable: true
+            } );
+
+// property.set = function( value ) { return this.properties[propertyName].value = value };
+// property.get = function() { return this.properties[propertyName].value; };
+
+            return this.onSetProperty( nodeID, propertyName, propertyValue );
         };
 
         this.onSetProperty = function( nodeID, propertyName, propertyValue ) {
 
+            var node = map[nodeID];
+            var property = node.properties[propertyName];
+
+            return property.set ? property.set.call( node, propertyValue ) : ( property.value = propertyValue );
         };
 
         this.onGetProperty = function( nodeID, propertyName ) {
         
+            var node = map[nodeID];
+            var property = node.properties[propertyName];
+
+            return property.get ? property.get.call( node ) : property.value;
         };
 
         this.onCreateMethod = function( nodeID, methodName ) {
@@ -72,10 +92,20 @@
 
         // Node
 
-        var Node = JavaScriptShard.Node = function( nodeID, source, mimeType ) {
-            this.id = nodeID; // private
+        var Node = JavaScriptShard.Node = function( nodeID, nodeName, source, mimeType ) {
+
+            this.id = nodeID; // TODO: make private
             this.parent = undefined;
+
+            this.name = nodeName;
+            this.source = source;
+            this.mimeType = mimeType;
+
+            this.properties = {};
+            this.methods = {};
+            this.events = {};
             this.children = [];
+
         };
 
         Node.prototype.createChild = function( nodeType, nodeName, source, mimeType ) {
@@ -86,8 +116,24 @@
             return vwf.createProperty( this.id, propertyName, propertyValue );
         };
 
-    };
+        Node.prototype.createMethod = function( methodName ) {
+            return vwf.createMethoe( this.id, methodName );
+        };
 
+        Node.prototype.createEvent = function( eventName ) {
+            return vwf.createEvent( this.id, eventName );
+        };
+
+        var Property = JavaScriptShard.Property = function( node, value ) {
+            this.node = node; // TODO: make private
+            this.value = value;
+            this.get = undefined;
+            this.set = undefined;
+        };
+
+        vwf.root = map[0] = new Node( 0 ); // TODO: symbol for global node ID
+    };
+    
     return window.vwf.js = JavaScriptShard;
 
 } ) ( window );
