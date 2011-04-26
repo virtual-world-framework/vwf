df960b6b994a29c17ed98eda5960f082a4d1e2fe
David Easter <david.easter@lmco.com>
2011-04-26 07:13:27 +0000

    Client with Model/View partition for Task 2 document. 2011-02-05 "client2 copy 0" version.
    git-svn-id: http://svn.virtualworldframework.com/trunk@60 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ Prototype/client2/vwf-model.js
@@ -0,0 +1,11 @@
+( function( vwf ) {
+
+    vwf.models.push( function( vwf ) {
+
+        this.onConstruct = function( nodeID, nodeName, nodeExtends, nodeImplements, nodeSource, nodeType ) {
+
+        };
+
+    } );
+
+} ) ( window.vwf );
