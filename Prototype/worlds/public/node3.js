2ebed9da1586c8def3eb417e200661b97f43fde3
David Easter <david.easter@lmco.com>
2011-05-02 20:37:52 +0000

    Placeholder types for 2-D and 3-D nodes.
    git-svn-id: http://svn.virtualworldframework.com/trunk@75 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ Prototype/worlds/public/node3.js
@@ -0,0 +1,11 @@
+﻿cb( {
+
+    extends: "http://vwf.example.com/types/node",
+
+    properties:
+    {
+        transform: [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 ],
+        enabled: true,
+    },
+
+} )
