2a069586529b026eaadef1688502252f0eb0b4ad
David Easter <david.easter@lmco.com>
2011-05-02 20:47:26 +0000

    Move test scene to its own type. Extend 3-D nodes from node3. Add a simple test script.
    git-svn-id: http://svn.virtualworldframework.com/trunk@77 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ Prototype/worlds/public/test1.js
@@ -0,0 +1,60 @@
+﻿cb( {
+    children:
+    {
+        scene:
+        {
+            name: "scene",
+                        
+            children:
+            {
+                earth:
+                {
+                    name: "earth",
+                                
+                    extends: "http://localhost/node3.js",
+
+                    properties:
+                    {
+                        transform: [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 2, 3 ],
+                        enabled: true,
+                        angle: 0,
+                    },
+                },
+
+                mars:
+                {
+                    name: "mars",
+                                
+                    extends: "http://localhost/node3.js",
+
+                    properties:
+                    {
+                        transform: [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 4, 5, 6 ],
+                        enabled: true,
+                        angle: 0,
+                    },
+
+                    scripts:
+                    [
+                        {
+                            text:
+                                "this.properties.angle.setter = function( value ) {" + " " +
+                                    "this.parent.earth.angle = value + 1;" + " " +
+                                "}",
+                            type: "application/javascript"
+                        },
+                    ],
+                },
+
+                venus:
+                {
+                    name: "venus",
+
+                    extends: "http://localhost/node3.js",
+                    source: "http://assets.vwf.example.com/venus.js",
+                    type: "x-model/scenejs",
+                },
+            },
+        },
+    },
+} )
