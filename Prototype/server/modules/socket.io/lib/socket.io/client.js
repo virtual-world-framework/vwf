89334fa444f3c9f5be59adf334b7e64d96c284e0
David Easter <david.easter@lmco.com>
2011-05-10 23:49:33 +0000

    Don't send the timeout duration in the heartbeat messages. The server is only expecting the sequence number. With the timeout value appended, the message isn't recognized as correct and the heartbeat never resets the server's timeout timer.
    git-svn-id: http://svn.virtualworldframework.com/trunk@138 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- Prototype/server/modules/socket.io/lib/socket.io/client.js
+++ Prototype/server/modules/socket.io/lib/socket.io/client.js
@@ -112,7 +112,7 @@ Client.prototype._payload = function(){
 Client.prototype._heartbeat = function(){
   var self = this;
   this._heartbeatInterval = setTimeout(function(){
-    self.send('~h~' + ++self._heartbeats);
+    self.send('~h~' + ++self._heartbeats); // self.send('~h~' + ++self._heartbeats + ' ' + self.options.heartbeatInterval);
     self._heartbeatTimeout = setTimeout(function(){
       self._onClose();
     }, self.options.timeout);
