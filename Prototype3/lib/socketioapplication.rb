c5c2d0a20fcd6ddedcc6712724fc97ba71828217
David Easter <david.easter@lmco.com>
2011-07-18 18:38:58 +0000

    2011-07-14 "socketio-compatible server" version (part 2).
    git-svn-id: http://svn.virtualworldframework.com/trunk@148 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ Prototype3/lib/socketioapplication.rb
@@ -0,0 +1,124 @@
+class SocketIOApplication < Rack::WebSocket::Application
+
+  attr_reader :env
+ 
+  def call env
+    @env = env
+    super
+  end
+
+  def onconnect
+    logger.info "SocketIOApplication#onconnect"
+  end
+
+  def onmessage message
+    logger.info "SocketIOApplication#onmessage"
+  end
+
+  def ondisconnect
+    logger.info "SocketIOApplication#ondisconnect"
+  end
+
+  def send message
+    logger.info "SocketIOApplication#send #{message}"
+    unless connected
+      queue message  # TODO
+    else
+      if message.is_a?( Array ) || message.is_a?( Hash )
+        send_serialization "~j~" + JSON.generate( message )  # TODO: errors
+      else
+        send_serialization message.to_s
+      end
+    end
+  end
+  
+  def broadcast message
+    logger.info "SocketIOApplication#broadcast #{message}"
+    send message  # TODO
+  end
+
+  def schedule_heartbeat
+    @heartbeat_interval = EventMachine::Timer.new 10 do  # TODO: options.heartbeatInterval
+      send_heartbeat ( @heartbeats += 1 ).to_s
+      @heartbeat_timeout = EventMachine::Timer.new 8 do  # TODO: options.timeout
+        logger.debug "SocketIOApplication#schedule_heartbeat timeout #{@heartbeats}"
+        # TODO: close
+      end
+    end
+  end
+
+  def on_heartbeat message
+    logger.debug "SocketIOApplication#on_heartbeat #{message}"
+    if message.to_i == @heartbeats
+      @heartbeat_timeout.cancel
+      schedule_heartbeat
+    end
+  end
+
+  def send_heartbeat message
+    logger.debug "SocketIOApplication#send_heartbeat #{message}"
+    send_serialization "~h~" + message
+  end
+
+  def on_serialization serialization
+    logger.debug "SocketIOApplication#on_serialization #{serialization}"
+    case serialization[0, 3]
+      when "~h~"
+        on_heartbeat serialization[3..-1]
+      when "~j~"
+        begin
+          onmessage JSON.parse serialization[3..-1]
+        rescue JSON::ParserError
+          # TODO: error
+        end
+      else
+        onmessage serialization[3..-1]
+    end
+  end
+
+  def send_serialization *serializations
+    logger.debug "SocketIOApplication#send_serialization #{serializations}"
+    data = serializations.reduce "" do |data, serialization|
+      data + "~m~" + serialization.length.to_s + "~m~" + serialization
+    end
+    logger.debug "Rack::WebSocket::Application#send_data #{data}"
+    send_data data
+  end
+
+  def on_open env
+    logger.debug "SocketIOApplication#on_open"
+    @session_id = rand( 1000000 ).to_s  # TODO: more random, map to server's actual session
+    @heartbeats = 0
+    send_serialization @session_id
+    schedule_heartbeat
+    onconnect
+  end
+
+  def on_message env, data
+    logger.debug "Rack::WebSocket::Application#on_data #{data}"
+    until data.empty?
+      if matchdata = data.match( /~m~(\d+)~m~/ )
+        match, length = *matchdata
+        on_serialization data[match.length, length.to_i]
+        data = data[match.length+length.to_i..-1] || ""
+      else
+        break # TODO: error
+      end
+    end
+  end
+  
+  def on_close env
+    logger.debug "SocketIOApplication#on_close"
+  end
+  
+  def on_error env, err
+    logger.debug "SocketIOApplication#on_error #{err}"
+  end
+
+private
+
+  def logger
+    @env["rack.logger"] || Object.new
+  end
+  
+end
