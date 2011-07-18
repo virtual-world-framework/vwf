942baf3ad5ed7dee98dae120e57c4ea3544bc761
David Easter <david.easter@lmco.com>
2011-07-18 18:49:41 +0000

    2011-07-17 "cleaned" (1) version.
    git-svn-id: http://svn.virtualworldframework.com/trunk@150 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- Prototype3/lib/server/application_pattern.rb
+++ Prototype3/lib/server/application_pattern.rb
@@ -18,31 +18,31 @@ class Server::ApplicationPattern
 
   end
 
-  # incoming path                                       application_path        public_path             application     session         private_path            
+  # incoming path                                       public_path             application     session         private_path            
 
-  # /path/to/component                                  "/path/to/component"    "/path/to/component"    "index.vwf"     nil             nil                     
-  # /path/to/component/                                 "/path/to/component"    "/path/to/component"    "index.vwf"     nil             nil                     
-  # /path/to/component/path/to/client/file              "/path/to/component"    "/path/to/component"    "index.vwf"     nil             "path/to/client/file"   
-  # /path/to/component/path/to/component/file           "/path/to/component"    "/path/to/component"    "index.vwf"     nil             "path/to/component/file"
-  # /path/to/component/socket/path                      "/path/to/component"    "/path/to/component"    "index.vwf"     nil             "socket/path"           
+  # /path/to/component                                  "/path/to/component"    "index.vwf"     nil             nil                     
+  # /path/to/component/                                 "/path/to/component"    "index.vwf"     nil             nil                     
+  # /path/to/component/path/to/client/file              "/path/to/component"    "index.vwf"     nil             "path/to/client/file"   
+  # /path/to/component/path/to/component/file           "/path/to/component"    "index.vwf"     nil             "path/to/component/file"
+  # /path/to/component/socket/path                      "/path/to/component"    "index.vwf"     nil             "socket/path"           
 
-  # /path/to/component.vwf                              "/path/to/component.vwf" "/path/to"             "component.vwf" nil             nil                     
-  # /path/to/component.vwf/                             "/path/to/component.vwf" "/path/to"             "component.vwf" nil             nil                     
-  # /path/to/component.vwf/path/to/client/file          "/path/to/component.vwf" "/path/to"             "component.vwf" nil             "path/to/client/file"   
-  # /path/to/component.vwf/path/to/component/file       "/path/to/component.vwf" "/path/to"             "component.vwf" nil             "path/to/component/file"
-  # /path/to/component.vwf/socket/path                  "/path/to/component.vwf" "/path/to"             "component.vwf" nil             "socket/path"           
+  # /path/to/component.vwf                              "/path/to"              "component.vwf" nil             nil                     
+  # /path/to/component.vwf/                             "/path/to"              "component.vwf" nil             nil                     
+  # /path/to/component.vwf/path/to/client/file          "/path/to"              "component.vwf" nil             "path/to/client/file"   
+  # /path/to/component.vwf/path/to/component/file       "/path/to"              "component.vwf" nil             "path/to/component/file"
+  # /path/to/component.vwf/socket/path                  "/path/to"              "component.vwf" nil             "socket/path"           
 
-  # /path/to/component/session                          "/path/to/component"    "/path/to/component"    "index.vwf"     "session"       nil                     
-  # /path/to/component/session/                         "/path/to/component"    "/path/to/component"    "index.vwf"     "session"       nil                     
-  # /path/to/component/session/path/to/client/file      "/path/to/component"    "/path/to/component"    "index.vwf"     "session"       "path/to/client/file"   
-  # /path/to/component/session/path/to/component/file   "/path/to/component"    "/path/to/component"    "index.vwf"     "session"       "path/to/component/file"
-  # /path/to/component/session/socket/path              "/path/to/component"    "/path/to/component"    "index.vwf"     "session"       "socket/path"           
+  # /path/to/component/session                          "/path/to/component"    "index.vwf"     "session"       nil                     
+  # /path/to/component/session/                         "/path/to/component"    "index.vwf"     "session"       nil                     
+  # /path/to/component/session/path/to/client/file      "/path/to/component"    "index.vwf"     "session"       "path/to/client/file"   
+  # /path/to/component/session/path/to/component/file   "/path/to/component"    "index.vwf"     "session"       "path/to/component/file"
+  # /path/to/component/session/socket/path              "/path/to/component"    "index.vwf"     "session"       "socket/path"           
 
-  # /path/to/component.vwf/session                      "/path/to/component.vwf" "/path/to"             "component.vwf" "session"       nil                     
-  # /path/to/component.vwf/session/                     "/path/to/component.vwf" "/path/to"             "component.vwf" "session"       nil                     
-  # /path/to/component.vwf/session/path/to/client/file  "/path/to/component.vwf" "/path/to"             "component.vwf" "session"       "path/to/client/file"   
-  # /path/to/component.vwf/session/path/to/component/file "/path/to/component.vwf" "/path/to"           "component.vwf" "session"       "path/to/component/file"
-  # /path/to/component.vwf/session/socket/path          "/path/to/component.vwf" "/path/to"             "component.vwf" "session"       "socket/path"           
+  # /path/to/component.vwf/session                      "/path/to"              "component.vwf" "session"       nil                     
+  # /path/to/component.vwf/session/                     "/path/to"              "component.vwf" "session"       nil                     
+  # /path/to/component.vwf/session/path/to/client/file  "/path/to"              "component.vwf" "session"       "path/to/client/file"   
+  # /path/to/component.vwf/session/path/to/component/file "/path/to"            "component.vwf" "session"       "path/to/component/file"
+  # /path/to/component.vwf/session/socket/path          "/path/to"              "component.vwf" "session"       "socket/path"           
 
   def match path
 
@@ -62,16 +62,11 @@ class Server::ApplicationPattern
       application = "index.vwf" + extension  # TODO: configuration parameter for default application name
     end
 
-# puts "#{path} * #{public_path} * #{application} * #{extension} * #{segments}"
-
     if extension
 
       session = segments.shift if session?( segments.first )
-      # application = nil if segments.empty? && file_url
-      # socket = File.join( segments.shift segments.length ) if socket?( segments.first )
       private_path = File.join( segments.shift segments.length ) unless segments.empty?
 
-      # Match.new [ application_path, application, session, socket, private_path ]
       Match.new [ public_path, application, session, private_path ]
 
     end
@@ -79,13 +74,6 @@ class Server::ApplicationPattern
     # TODO: which parts are URL paths and which are filesystem paths? don't use File.join on URL paths
 
   end
-
-  # Assemble the pieces back into a path.
-
-#   def self.assemble application_path, public_path, application, session, private_path
-# puts "assemble: #{application_path} * #{public_path} * #{application} * #{session} * #{private_path}: #{ application_path == "/" ? "" : application_path }#{ session && "/" + session }#{ application && "/" }#{ public_path }"
-#     "#{ application_path == "/" ? "" : application_path }#{ session && "/" + session }#{ application && "/" }#{ private_path }"
-#   end
 
 private
 
@@ -122,9 +110,5 @@ private
   def session? segment
     segment =~ /^[0-9A-Za-z]{16}$/
   end
-
-  # def socket? segment
-  #   segment == "socket" || segment == "websocket"
-  # end
 
 end
