8efff76276fc4b81becc731f0c88cd97414dc427
Scott Haynes <scott.r.haynes@lmco.com>
2011-08-18 18:44:00 +0000

    updated the scene, node3, camera, and created the light and camera components
    git-svn-id: http://svn.virtualworldframework.com/trunk@275 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/vwf-view-html.js
+++ support/client/vwf-view-html.js
@@ -377,7 +377,7 @@
                     event.preventDefault();
                 }
                 // Allow Only: keyboard 0-9, numpad 0-9, backspace, tab, left arrow, right arrow, delete
-                if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105) || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 46) {
+                if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105) || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 46 || event.keyCode == 190) {
                     // Allow normal operation
                 } else {
                     // Prevent the rest
