dbe9f2238fad60b7c055a6e0a29e6790ea8a78af
David Easter <david.easter@lmco.com>
2011-08-28 22:46:52 +0000

    Move client files into lib to make room for test.
    git-svn-id: http://svn.virtualworldframework.com/trunk@287 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- lib/vwf/application.rb
+++ lib/vwf/application.rb
@@ -6,15 +6,15 @@ class VWF::Application
 
       map "/" do
         run Rack::Cascade.new [
-          Reflector.new,                                          # The WebSocket reflector  # TODO: not for session==nil?  # Reflector.new( :debug => true, :backend => { :debug => true } ),
-          Rack::File.new( VWF.settings.client ),                  # Client files from ^/support/client
-          Rack::File.new( File.join VWF.settings.public, root ),  # Public content from ^/public  # TODO: will match public_path/index.html which we don't really want
-          Component.new( File.join VWF.settings.public, root )    # A component, possibly from a template or as JSONP  # TODO: before public for serving plain json as jsonp?
+          Reflector.new,                                                        # The WebSocket reflector  # TODO: not for session==nil?  # Reflector.new( :debug => true, :backend => { :debug => true } ),
+          Rack::File.new( File.join VWF.settings.support, "client/lib" ),       # Client files from ^/support/client/lib
+          Rack::File.new( File.join VWF.settings.public, root ),                # Public content from ^/public  # TODO: will match public_path/index.html which we don't really want
+          Component.new( File.join VWF.settings.public, root )                  # A component, possibly from a template or as JSONP  # TODO: before public for serving plain json as jsonp?
         ]
       end
 
       map "/socket.io" do
-        run Rack::File.new( File.join( VWF.settings.support, "socket.io-client" ) ) # socket.io client files from ^/support/socket.io-client
+        run Rack::File.new( File.join VWF.settings.support, "socket.io-client" ) # socket.io client files from ^/support/socket.io-client
       end
 
       map "/admin" do
