ec2c1c5cce6f1bb20c32c8842c66c881dbe70218
David Easter <david.easter@lmco.com>
2011-08-28 22:13:32 +0000

    Move the socket.io client files out of the vwf client and into its own support directory.
    git-svn-id: http://svn.virtualworldframework.com/trunk@284 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- lib/vwf/application.rb
+++ lib/vwf/application.rb
@@ -4,10 +4,6 @@ class VWF::Application
 
     @app = Rack::Builder.new do
 
-      map "/admin" do
-        run Admin
-      end
-
       map "/" do
         run Rack::Cascade.new [
           Reflector.new,                                          # The WebSocket reflector  # TODO: not for session==nil?  # Reflector.new( :debug => true, :backend => { :debug => true } ),
@@ -15,6 +11,14 @@ class VWF::Application
           Rack::File.new( File.join VWF.settings.public, root ),  # Public content from ^/public  # TODO: will match public_path/index.html which we don't really want
           Component.new( File.join VWF.settings.public, root )    # A component, possibly from a template or as JSONP  # TODO: before public for serving plain json as jsonp?
         ]
+      end
+
+      map "/socket.io" do
+        run Rack::File.new( File.join( VWF.settings.support, "socket.io-client" ) ) # socket.io client files from ^/support/socket.io-client
+      end
+
+      map "/admin" do
+        run Admin
       end
 
     end
