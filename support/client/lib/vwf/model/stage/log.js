a9d035f7ae76cb6250d5853a6e29b40d3eaa044c
David Easter <david.easter@lmco.com>
2011-09-29 06:27:44 +0000

    Model pipeline stages:
      - log: automatically log incoming and outgoing messages from the model (or stage) downpipe. For debug mode, the kernel can automatically insert the logging stage in each model pipeline, or between each stage in each pipeline, saving the models and stages from needing to log each call individually.
    
      - map: translate between nodeIDs used in the kernel and object references or object ids for the corresponding entities in a model. The model will receive model messages containing direct messages to its objects and may send kernel messages with direct references to model objects. This saves the model the effort of maintaining a separate translation table and performing translations on every call.
    
    
    git-svn-id: http://svn.virtualworldframework.com/trunk@327 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ support/client/lib/vwf/model/stage/log.js
@@ -0,0 +1,204 @@
+define( [ "module", "vwf/model/stage" ], function( module, stage ) {
+
+    return stage.load( module, {
+
+        // == Module Definition ====================================================================
+
+        initialize: function() {
+            this.logger = this.model.logger; // steal the model's logger since we're logging for it
+        },
+        
+    }, function( kernel_function ) {
+
+        // == Kernel API ===========================================================================
+
+        return function() {
+
+            var logees = Array.prototype.slice.call( arguments );
+
+            switch ( kernel_function ) {
+
+                case "createNode":
+                    objectIsComponent( logees[0] ) && ( logees[0] = JSON.stringify( loggableComponent( logees[0] ) ) ); // component_uri_or_json_or_object
+                    break;
+
+                case "createProperty":
+                    logees[3] && ( logees[3] = loggableScript( logees[3] ) ); // propertyGet
+                    logees[4] && ( logees[4] = loggableScript( logees[4] ) ); // propertySet
+                    break;
+
+                case "execute":
+                    logees[1] && ( logees[1] = loggableScript( logees[1] ) ); // scriptText
+                    break;
+
+                case "time":
+                    logees = undefined; // no logging for kernel.time()
+                    break;
+
+            }
+
+            logees && this.logger.debug.apply( this.logger, [ kernel_function ].concat( logees ) );
+            return this.kernel[kernel_function].apply( this.kernel, arguments );
+
+        };
+        
+    }, function( model_function ) {
+
+        // == Model API ============================================================================
+
+        return function() {
+
+            if ( this.model[model_function] ) {
+
+                var logees = Array.prototype.slice.call( arguments );
+
+                switch ( model_function ) {
+
+                    case "creatingProperty":
+                        logees[3] && ( logees[3] = loggableScript( logees[3] ) ); // propertyGet
+                        logees[4] && ( logees[4] = loggableScript( logees[4] ) ); // propertySet
+                        break;
+
+                    case "executing":
+                        logees[1] && ( logees[1] = loggableScript( logees[1] ) ); // scriptText
+                        break;
+
+                }
+
+                logees && this.logger.debug.apply( this.logger, [ model_function ].concat( logees ) );
+                return this.model[model_function].apply( this.model, arguments );
+
+            }
+
+        };
+
+    } );
+
+    // == Private functions ========================================================================
+
+    function objectIsComponent( candidate ) {  // TODO: this was copied from vwf.js; find a way to share (use the log stage for incoming logging too?)
+
+        var componentAttributes = [
+            "extends",
+            "implements",
+            "source",
+            "type",
+            "properties",
+            "methods",
+            "events",
+            "children",
+            "scripts",
+        ];
+
+        var isComponent = false;
+
+        if ( ( typeof candidate == "object" || candidate instanceof Object ) && candidate != null ) {
+
+            componentAttributes.forEach( function( attributeName ) {
+                isComponent = isComponent || Boolean( candidate[attributeName] );
+            } );
+
+        }
+            
+        return isComponent; 
+    };
+
+    function loggableComponent( component ) {  // TODO: this was copied from vwf.js; find a way to share (use the log stage for incoming logging too?)
+
+        var loggable = {};
+
+        for ( var elementName in component ) {
+
+            switch ( elementName ) {
+
+                case "properties":
+
+                    loggable.properties = {};
+
+                    for ( var propertyName in component.properties ) {
+
+                        var componentPropertyValue = component.properties[propertyName];
+                        var loggablePropertyValue = loggable.properties[propertyName] = {};
+
+                        if ( valueHasAccessors( componentPropertyValue ) ) {
+                            for ( var propertyElementName in componentPropertyValue ) {
+                                if ( propertyElementName == "set" || propertyElementName == "get" ) {
+                                    loggablePropertyValue[propertyElementName] = "...";
+                                } else {
+                                    loggablePropertyValue[propertyElementName] = componentPropertyValue[propertyElementName];
+                                }
+                            }
+                        } else {
+                            loggable.properties[propertyName] = componentPropertyValue;
+                        }
+
+                    }
+
+                    break;
+
+                case "children":
+
+                    loggable.children = {};
+
+                    for ( var childName in component.children ) {
+                        loggable.children[childName] = {};
+                    }
+
+                    break;
+
+                case "scripts":
+
+                    loggable.scripts = [];
+
+                    component.scripts.forEach( function( script ) {
+
+                        var loggableScript = {};
+
+                        for ( var scriptElementName in script ) {
+                            loggableScript[scriptElementName] = scriptElementName == "text" ? "..." : script[scriptElementName];
+                        }
+
+                        loggable.scripts.push( loggableScript );
+
+                    } );
+
+                    break;
+
+                default:
+
+                    loggable[elementName] = component[elementName];
+
+                    break;
+            }
+
+        }
+
+        return loggable;
+    }
+
+    function valueHasAccessors( candidate ) {  // TODO: this was copied from vwf.js; find a way to share (use the log stage for incoming logging too?)
+
+        var accessorAttributes = [
+            "get",
+            "set",
+            "value",
+        ];
+
+        var hasAccessors = false;
+
+        if ( ( typeof candidate == "object" || candidate instanceof Object ) && candidate != null ) {
+
+            accessorAttributes.forEach( function( attributeName ) {
+                hasAccessors = hasAccessors || Boolean( candidate[attributeName] );
+            } );
+
+        }
+            
+        return hasAccessors; 
+    }
+
+    function loggableScript( scriptText ) {
+        return ( scriptText || "" ).replace( /\s+/g, " " ).substring( 0, 100 );
+    }
+
+} );
---
+++ support/client/lib/vwf/model/stage/map.js
@@ -0,0 +1,225 @@
+define( [ "module", "vwf/model/stage" ], function( module, stage ) {
+
+    // vwf/model/stage/map.js translates between kernel-side nodeIDs and model-side objects or ids.
+
+    return stage.load( module, {
+
+        // == Module Definition ====================================================================
+
+        // -- initialize ---------------------------------------------------------------------------
+
+        initialize: function( kernel, model, object_id ) {
+
+            this.kernel_to_model = {}; // maps nodeID => model object
+            this.model_to_kernel = {}; // maps model object => nodeID
+
+            if ( typeof object_id == "function" || object_id instanceof Function ) {
+                this.object_id = object_id;
+            } else if ( typeof object_id == "string" || object_id instanceof String ) {
+                this.object_id = function( node ) { return node[object_id] };
+            } else {
+                this.object_id = function( node ) { return node }; // will use node's toString()
+            }
+
+        },
+
+        // == Kernel API ===========================================================================
+
+        // -- createNode ---------------------------------------------------------------------------
+
+        createNode: function( component_uri_or_object, callback_nodeID_prototypeID ) {
+            return this.kernel.createNode( component_uri_or_object, callback_nodeID_prototypeID );  // TODO: remap callback parameters
+        },
+
+        // TODO: deleteNode
+
+        // -- addChild -----------------------------------------------------------------------------
+
+        addChild: function( node, child, childName ) {
+            return this.kernel.addChild(
+                this.model_to_kernel[this.object_id(node)] || node, this.model_to_kernel[this.object_id(child)] || child,
+                childName );
+        },
+
+        // -- removeChild --------------------------------------------------------------------------
+
+        removeChild: function( node, child ) {
+            return this.kernel.removeChild(
+                this.model_to_kernel[this.object_id(node)] || node, this.model_to_kernel[this.object_id(child)] || child );
+        },
+
+        // -- parent -------------------------------------------------------------------------------
+
+        parent: function( node ) {
+            return this.kernel.parent( this.model_to_kernel[this.object_id(node)] || node );
+        },
+
+        // -- children -----------------------------------------------------------------------------
+
+        children: function( node ) {
+            return this.kernel.children( this.model_to_kernel[this.object_id(node)] || node );
+        },
+
+        // -- name ---------------------------------------------------------------------------------
+
+        name: function( node ) {
+            return this.kernel.name( this.model_to_kernel[this.object_id(node)] || node );
+        },
+
+        // -- createProperty -----------------------------------------------------------------------
+
+        createProperty: function( node, propertyName, propertyValue ) {
+            return this.kernel.createProperty( this.model_to_kernel[this.object_id(node)] || node,
+                propertyName, propertyValue );
+        },
+
+        // TODO: deleteProperty
+
+        // -- setProperty --------------------------------------------------------------------------
+
+        setProperty: function( node, propertyName, propertyValue ) {
+            return this.kernel.setProperty( this.model_to_kernel[this.object_id(node)] || node,
+                propertyName, propertyValue );
+        },
+
+        // -- getProperty --------------------------------------------------------------------------
+
+        getProperty: function( node, propertyName, propertyValue ) {
+            return this.kernel.getProperty( this.model_to_kernel[this.object_id(node)] || node,
+                propertyName, propertyValue );
+        },
+
+        // -- createMethod -------------------------------------------------------------------------
+
+        createMethod: function( node, methodName ) {
+            return this.kernel.createMethod( this.model_to_kernel[this.object_id(node)] || node,
+                methodName );
+        },
+
+        // TODO: deleteMethod
+
+        // -- callMethod ---------------------------------------------------------------------------
+
+        callMethod: function( node, methodName ) { // TODO: parameters
+            return this.kernel.callMethod( this.model_to_kernel[this.object_id(node)] || node,
+                methodName );
+        },
+    
+        // TODO: createEvent, deleteEvent, addEventListener, removeEventListener, fireEvent
+
+        // -- execute ------------------------------------------------------------------------------
+
+        execute: function( node, scriptText, scriptType ) {
+            return this.kernel.execute( this.model_to_kernel[this.object_id(node)] || node,
+                scriptText, scriptType );
+        },
+
+        // -- time ---------------------------------------------------------------------------------
+
+        time: function() {
+            return this.kernel.time();
+        },
+
+        // == Model API ============================================================================
+
+        // -- creatingNode -------------------------------------------------------------------------
+
+        creatingNode: function( nodeID, nodeExtendsID, nodeImplementsIDs, nodeSource, nodeType ) {
+
+            var node = this.model.creatingNode && this.model.creatingNode( nodeID,
+                this.kernel_to_model[nodeExtendsID] || nodeExtendsID,
+                nodeImplementsIDs, nodeSource, nodeType );  // TODO: remap nodeImplementsIDs array values
+
+            if ( node !== undefined ) {
+                this.kernel_to_model[nodeID] = node;
+                this.model_to_kernel[this.object_id(node)] = nodeID;
+            }
+
+            return undefined; // creatingNode doesn't return anything to the kernel
+        },
+
+        // TODO: deletingNode
+
+        // -- addingChild --------------------------------------------------------------------------
+
+        addingChild: function( nodeID, childID, childName ) {
+            return this.model.addingChild && this.model.addingChild(
+                this.kernel_to_model[nodeID] || nodeID, this.kernel_to_model[childID] || childID, childName );
+        },
+
+        // -- removingChild ------------------------------------------------------------------------
+
+        removingChild: function( nodeID, childID ) {
+            return this.model.removingChild && this.model.removingChild(
+                this.kernel_to_model[nodeID] || nodeID, this.kernel_to_model[childID] || childID );
+        },
+
+        // -- parenting ----------------------------------------------------------------------------
+
+        parenting: function( nodeID ) {
+            return this.model.parenting && this.model.parenting( this.kernel_to_model[nodeID] || nodeID );
+        },
+
+        // -- childrening --------------------------------------------------------------------------
+
+        childrening: function( nodeID ) {
+            return this.model.childrening && this.model.childrening( this.kernel_to_model[nodeID] || nodeID );
+        },
+
+        // -- naming -------------------------------------------------------------------------------
+
+        naming: function( nodeID ) {
+            return this.model.naming && this.model.naming( this.kernel_to_model[nodeID] || nodeID );
+        },
+
+        // -- creatingProperty ---------------------------------------------------------------------
+
+        creatingProperty: function( nodeID, propertyName, propertyValue ) {
+            return this.model.creatingProperty && this.model.creatingProperty( this.kernel_to_model[nodeID] || nodeID,
+                propertyName, propertyValue );
+        },
+
+        // TODO: deletingProperty
+
+        // -- settingProperty ----------------------------------------------------------------------
+
+        settingProperty: function( nodeID, propertyName, propertyValue ) {
+            return this.model.settingProperty && this.model.settingProperty( this.kernel_to_model[nodeID] || nodeID,
+                propertyName, propertyValue );
+        },
+
+        // -- gettingProperty ----------------------------------------------------------------------
+
+        gettingProperty: function( nodeID, propertyName, propertyValue ) {
+            return this.model.gettingProperty && this.model.gettingProperty( this.kernel_to_model[nodeID] || nodeID,
+                propertyName, propertyValue );
+        },
+
+        // -- creatingMethod -----------------------------------------------------------------------
+
+        creatingMethod: function( nodeID, methodName ) {
+            return this.model.creatingMethod && this.model.creatingMethod( this.kernel_to_model[nodeID] || nodeID,
+                methodName );
+        },
+
+        // TODO: deletingMethod
+
+        // -- callingMethod ------------------------------------------------------------------------
+
+        callingMethod: function( nodeID, methodName ) { // TODO: parameters
+            return this.model.callingMethod && this.model.callingMethod( this.kernel_to_model[nodeID] || nodeID,
+                methodName );
+        },
+
+        // TODO: creatingEvent, deletingEvent, firingEvent
+
+        // -- executing ----------------------------------------------------------------------------
+
+        executing: function( nodeID, scriptText, scriptType ) {
+            return this.model.executing && this.model.executing( this.kernel_to_model[nodeID] || nodeID,
+                scriptText, scriptType );
+        },
+
+    } );
+
+} );
