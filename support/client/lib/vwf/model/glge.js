66e8cd9d08545aa97be4d9422105593d3f6ffb40
David Easter <david.easter@lmco.com>
2011-09-30 18:08:09 +0000

    Start a GLGE model (currently delegating to the GLGE view) to properly expose properties that GLGE owns so that the GLGE view doesn't need to copy any potentially needed properties into VWF. This is primarily for the vertex data in support of the physics simulation.
    git-svn-id: http://svn.virtualworldframework.com/trunk@332 775f9103-8eee-4f94-88be-65e0ca6d8b75

---
+++ support/client/lib/vwf/model/glge.js
@@ -0,0 +1,53 @@
+define( [ "module", "vwf/model" ], function( module, model ) {
+
+    // vwf/model/glge.js is an interface to the GLGE WebGL scene manager.
+
+    // For historical reasons yet to be resolved, the GLGE model code currently resides in
+    // vwf-view.glge.js intermixed with the view code. This driver is a gross hack to delegate model
+    // calls to the appropriate parts of the GLGE view.
+
+    return model.load( module, {
+
+        // == Module Definition ====================================================================
+
+        // -- initialize ---------------------------------------------------------------------------
+
+        initialize: function() {
+            this.glge_view = undefined;
+        },
+
+        // == Model API ============================================================================
+
+        // -- gettingProperty ----------------------------------------------------------------------
+
+        gettingProperty: function( nodeID, propertyName, propertyValue ) {
+
+            if ( this.glge_view || ( this.glge_view = find_glge_view( this.kernel ) ) ) {
+                if ( propertyName != "playing" && propertyName != "looping" && propertyName != "speed" ) {  // vwf-view-glge expects to vwf.getProperty() on these to get to default property settings
+                    return this.glge_view.gotProperty( nodeID, propertyName, propertyValue );
+                }
+            }
+
+        },
+
+    } );
+
+    // -- find_glge_view ---------------------------------------------------------------------------
+
+    function find_glge_view( kernel ) {
+
+        // Walk the pipeline backwards to the kernel.
+
+        while ( kernel.kernel ) {
+            kernel = kernel.kernel;
+        }
+
+        // Locate the GLGE view in the kernel's view list.
+
+        return kernel.views.reduce( function( dummy, view ) {
+            return view.namespace == "vwf.view.glge" ? view : undefined;
+        }, undefined );
+
+    }
+
+} );
