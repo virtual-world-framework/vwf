60479ab4b74107fd4785ea11d53f6beca05ffbaf
David Easter <david.easter@lmco.com>
2011-10-26 00:30:43 +0000

    Add parameters in comments.
    git-svn-id: http://svn.virtualworldframework.com/branches/integration@419 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/lib/vwf/api/kernel.js
+++ support/client/lib/vwf/api/kernel.js
@@ -1,22 +1,22 @@
 define( {
 
-    createNode: [],
+    createNode: [ /* component_uri_or_json_or_object, callback, childName /- TODO: hack -/ */ ],
     /* TODO: deleteNode, */
 
-    addChild: [],
-    removeChild: [],
-    parent: [],
-    children: [],
-    name: [],
+    addChild: [ /* nodeID, childID, childName */ ],
+    removeChild: [ /* nodeID, childID */ ],
+    parent: [ /* nodeID */ ],
+    children: [ /* nodeID */ ],
+    name: [ /* nodeID */ ],
 
-    createProperty: [],
+    createProperty: [ /* nodeID, propertyName, propertyValue, propertyGet, propertySet */ ],
     /* TODO: deleteProperty, */
-    setProperty: [],
-    getProperty: [],
+    setProperty: [ /* nodeID, propertyName, propertyValue */ ],
+    getProperty: [ /* nodeID, propertyName */ ],
 
-    createMethod: [],
+    createMethod: [ /* nodeID, methodName */ ],
     /* TODO: deleteMethod, */
-    callMethod: [],
+    callMethod: [ /* nodeID, methodName /- [, parameter1, parameter2, ... ] -/ */ ],
 
     /* TODO: createEvent, */
     /* TODO: deleteEvent, */
@@ -24,7 +24,7 @@ define( {
     /* TODO: removeEventListener, */
     /* TODO: fireEvent, */
 
-    execute: [],
+    execute: [ /* nodeID, scriptText, scriptType */ ],
 
     time: [],
 
