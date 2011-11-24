d2df545caf237f9fbb2db6621f79c74454ad0a74
David Easter <david.easter@lmco.com>
2011-11-24 00:12:05 +0000

    deletingNode().
    git-svn-id: http://svn.virtualworldframework.com/branches/integration@554 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/lib/vwf/model/object.js
+++ support/client/lib/vwf/model/object.js
@@ -38,7 +38,7 @@ define( [ "module", "vwf/model" ], function( module, model ) {
         // -- deletingNode -------------------------------------------------------------------------
 
         deletingNode: function( nodeID ) {
-            this.logger.warn( "deletingNode", "unimplemented" );
+            delete this.objects[nodeID];
         },
 
         // -- prototyping --------------------------------------------------------------------------
