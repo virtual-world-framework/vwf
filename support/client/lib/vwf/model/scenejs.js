d6be19b6fb519b0f4c49656795d46d959eb58634
Jocelyn Sabounji <jocelyn.sabounji@lmco.com>
2012-03-07 13:32:42 +0000

    Added LM Copyright and Apache License Header
    git-svn-id: http://svn.virtualworldframework.com/branches/integration@927 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/lib/vwf/model/scenejs.js
+++ support/client/lib/vwf/model/scenejs.js
@@ -1,3 +1,16 @@
+/*Copyright 2012 Lockheed Martin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 define( [ "module", "vwf/model" ], function( module, model ) {
 
     // vwf/model/scenejs.js is a placeholder for a 3-D scene manager.
