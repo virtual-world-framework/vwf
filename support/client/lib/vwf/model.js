1df74de117445abaa1064d8ff4d09d28368ac9ca
Jocelyn Sabounji <jocelyn.sabounji@lmco.com>
2012-03-08 17:48:05 +0000

    Removing the LM Copyright and License Header
    git-svn-id: http://svn.virtualworldframework.com/branches/integration@948 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/lib/vwf/model.js
+++ support/client/lib/vwf/model.js
@@ -1,16 +1,4 @@
-/*Copyright 2012 Lockheed Martin
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
 define( [ "module", "vwf/api/kernel", "vwf/api/model", "vwf-proxy" ], function( module, kernel_api, model_api ) {  // TODO: remove explicit reference to vwf / require( "vwf-proxy" )
 
     // vwf/model.js is the common implementation of all Virtual World Framework models. Each model
