5ae159c7eb4d73c8838c5ec3058c638fe4cc4628
Scott Haynes <scott.r.haynes@lmco.com>
2012-04-11 18:05:07 +0000

    files updated to get camera movement based on time
    git-svn-id: http://svn.virtualworldframework.com/branches/features/cameraSmoothing@1308 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/lib/vwf/view/glge.js
+++ support/client/lib/vwf/view/glge.js
@@ -74,6 +74,7 @@ define( [ "module", "vwf/view" ], function( module, view ) {
                 window.onkeydown = function (event) {
                     var key = undefined;
                     var validKey = false;
+                    var keyAlreadyDown = false;
                     switch (event.keyCode) {
                         case 17:
                         case 16:
@@ -83,6 +84,7 @@ define( [ "module", "vwf/view" ], function( module, view ) {
                             break;
                         default:
                             key = getKeyValue.call( glgeView, event.keyCode);
+                            keyAlreadyDown = !!glgeView.keyStates.keysDown[key.key];
                             glgeView.keyStates.keysDown[key.key] = key;
                             validKey = true;
                             break;
@@ -95,7 +97,7 @@ define( [ "module", "vwf/view" ], function( module, view ) {
                     glgeView.keyStates.mods.meta = event.metaKey;
 
                     var sceneNode = glgeView.state.scenes[glgeView.state.sceneRootID];
-                    if (validKey && sceneNode /*&& Object.keys( glgeView.keyStates.keysDown ).length > 0*/) {
+                    if (validKey && sceneNode && !keyAlreadyDown /*&& Object.keys( glgeView.keyStates.keysDown ).length > 0*/) {
                         //var params = JSON.stringify( glgeView.keyStates );
                         glgeView.kernel.dispatchEvent(sceneNode.ID, "keyDown", [glgeView.keyStates]);
                     }
@@ -453,11 +455,11 @@ define( [ "module", "vwf/view" ], function( module, view ) {
                                         currentObj = currentObj.parent;
                                 } 
                                 if ( colladaObj ) {
-                                    recurseGroup.call( this, colladaObj, 0 );
+                                    recurseGroup.call( sceneView, colladaObj, 0 );
                                 }
                             }                
                         } else if ( atlDown && !ctrlDown ) {
-                            recurseGroup.call( this, glgeObj, 0 ); 
+                            recurseGroup.call( sceneView, glgeObj, 0 ); 
                         }
                     }
                 }
