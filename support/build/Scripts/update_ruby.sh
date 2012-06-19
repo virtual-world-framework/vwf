b1a04ac8355c6f18ccf6efe0d7c18f386404c7a6
Michael Vacirca <michael.j.vacirca@lmco.com>
2012-06-19 18:54:39 +0000

    Adding "cache" folder reference instead of support/build
    git-svn-id: http://svn.virtualworldframework.com/vwf/development@1963 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/build/Scripts/update_ruby.sh
+++ support/build/Scripts/update_ruby.sh
@@ -20,25 +20,43 @@
 # export http_proxy=http://proxy-server.mycorp.com:3128/
 ###################################################################################################
 
-# Download the latest VWF Master Branch Baseline to the local system
-if [ ! -d "/support/build/ruby-1.8.7-p357-i386-mingw32" ];then
+# Download the latest VWF Ruby and DevKit to the local system
+if [ ! -d "../cache" ];then
 		echo "If you are behind a proxy, please make sure your http_proxy variable is set for Cygwin. Otherwise, the build cannot continue. You may set your proxy using the bash command export http_proxy=http://proxy-server.mycorp.com:3128/
 		
 		"
-		cd support/build
+		# Download Ruby 1.8.7 to Cache
+		cd ..
+		mkdir cache
+		cd cache
 		curl -k https://cloud.github.com/downloads/virtual-world-framework/vwf/ruby-1.8.7-p357-i386-mingw32.tar.gz --O ruby-1.8.7-p357-i386-mingw32.tar.gz --proxy $http_proxy
 		tar -xvzf ruby-1.8.7-p357-i386-mingw32.tar.gz
-		rm -rf ruby-1.8.7-p357-i386-mingw32.tar.gz
-		cd ../..
-fi
-
-if [ ! -d "/support/build/ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx" ];then
-		echo "If you are behind a proxy, please make sure your http_proxy variable is set for Cygwin. Otherwise, the build cannot continue. You may set your proxy using the bash command export http_proxy=http://proxy-server.mycorp.com:3128/
+		rm ruby-1.8.7-p357-i386-mingw32.tar.gz
 		
-		"
-		cd support/build
+		# Download DevKit to Cache
 		curl -k https://cloud.github.com/downloads/virtual-world-framework/vwf/ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz --O ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz --proxy $http_proxy
 		tar -xvzf ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz
-		rm -rf ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz
-		cd ../..
+		rm ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz
 fi
+	
+# if [ ! -d "support/build/ruby-1.8.7-p357-i386-mingw32" ];then
+		# echo "If you are behind a proxy, please make sure your http_proxy variable is set for Cygwin. Otherwise, the build cannot continue. You may set your proxy using the bash command export http_proxy=http://proxy-server.mycorp.com:3128/
+		
+		# "
+		# cd support/build
+		# curl -k https://cloud.github.com/downloads/virtual-world-framework/vwf/ruby-1.8.7-p357-i386-mingw32.tar.gz --O ruby-1.8.7-p357-i386-mingw32.tar.gz --proxy $http_proxy
+		# tar -xvzf ruby-1.8.7-p357-i386-mingw32.tar.gz
+		# rm ruby-1.8.7-p357-i386-mingw32.tar.gz
+		# cd ../..
+# fi
+
+# if [ ! -d "support/build/ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx" ];then
+		# echo "If you are behind a proxy, please make sure your http_proxy variable is set for Cygwin. Otherwise, the build cannot continue. You may set your proxy using the bash command export http_proxy=http://proxy-server.mycorp.com:3128/
+		
+		# "
+		# cd support/build
+		# curl -k https://cloud.github.com/downloads/virtual-world-framework/vwf/ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz --O ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz --proxy $http_proxy
+		# tar -xvzf ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz
+		# rm ruby-devkit-tdm-32-4.5.2-20111229-1559-sfx.tar.gz
+		# cd ../..
+# fi
