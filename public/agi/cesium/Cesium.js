006c5c782722dd461b5ca6ce05b83ee1054473c7
Michael Vacirca <michael.j.vacirca@lmco.com>
2012-06-21 21:45:24 +0000

    Updates catalog.html.erb to add connected value.  Adds space character to YAML file that is needed for parser, and fixed Windows EOL to Unix EOL on Cesium.js
    git-svn-id: http://svn.virtualworldframework.com/vwf/development@1992 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- public/agi/cesium/Cesium.js
+++ public/agi/cesium/Cesium.js
@@ -37778,246 +37778,8 @@ define('Scene/CentralBody',[
     return CentralBody;
 });
 
-/*! Copyright (c) 2010 Chris O'Hara <cohara87@gmail.com>. MIT Licensed */
-
-/*global define*/
-define('ThirdParty/Chain',[],function() {
-    var Chain = {};
-
-(function(exports) {
-
-    exports = exports || {};
-
-    var handlers = {}, createChain, add;
-
-    createChain = function (context, stack, lastMethod) {
-
-        var inHandler = context.halt = false;
-
-        //The default error handler
-        context.error = function (e) {
-            throw e;
-        };
-
-        //Run the next method in the chain
-        context.next = function (exit) {
-            if (exit) {
-                inHandler = false;
-            }
-            if (!context.halt && stack && stack.length) {
-                var args = stack.shift(), method = args.shift();
-                inHandler = true;
-                try {
-                    handlers[method].apply(context, [args, args.length, method]);
-                } catch (e) {
-                    context.error(e);
-                }
-            }
-            return context;
-        };
-
-        //Bind each method to the context
-        for (var alias in handlers) {
-            if (typeof context[alias] === 'function') {
-                continue;
-            }
-            (function (alias) {
-                context[alias] = function () {
-                    var args = Array.prototype.slice.call(arguments);
-                    if (alias === 'onError') {
-                        if (stack) {
-                            handlers.onError.apply(context, [args, args.length]);
-                            return context;
-                        }
-
-                        var new_context = {};
-                        handlers.onError.apply(new_context, [args, args.length]);
-                        return createChain(new_context, null, 'onError');
-                    }
-                    args.unshift(alias);
-                    if (!stack) {
-                        return createChain({}, [args], alias);
-                    }
-                    context.then = context[alias];
-                    stack.push(args);
-                    return inHandler ? context : context.next();
-                };
-            }(alias));
-        }
-
-        //'then' is an alias for the last method that was called
-        if (lastMethod) {
-            context.then = context[lastMethod];
-        }
-
-        //Used to call run(), chain() or another existing method when defining a new method
-        //See load.js (https://github.com/chriso/load.js/blob/master/load.js) for an example
-        context.call = function (method, args) {
-            args.unshift(method);
-            stack.unshift(args);
-            context.next(true);
-        };
-
-        return context.next();
-    };
-
-    //Add a custom method/handler (see below)
-    add = exports.addMethod = function (method /*, alias1, alias2, ..., callback */) {
-        var args = Array.prototype.slice.call(arguments),
-            handler = args.pop();
-        for (var i = 0, len = args.length; i < len; i++) {
-            if (typeof args[i] === 'string') {
-                handlers[args[i]] = handler;
-            }
-        }
-        //When no aliases have been defined, automatically add 'then<Method>'
-        //e.g. adding 'run' also adds 'thenRun' as a method
-        if (!--len) {
-            handlers['then' + method.substr(0,1).toUpperCase() + method.substr(1)] = handler;
-        }
-        createChain(exports);
-    };
-
-    //chain() - Run each function sequentially
-    add('chain', function (args) {
-        var self = this, next = function () {
-            if (self.halt) {
-                return;
-            } else if (!args.length) {
-                return self.next(true);
-            }
-            try {
-                if (null != args.shift().call(self, next, self.error)) {
-                    next();
-                }
-            } catch (e) {
-                self.error(e);
-            }
-        };
-        next();
-    });
-
-    //run() - Run each function in parallel and progress once all functions are complete
-    add('run', function (args, arg_len) {
-        var self = this, chain = function () {
-            if (self.halt) {
-                return;
-            } else if (!--arg_len) {
-                self.next(true);
-            }
-        };
-        var error = function (e) {
-            self.error(e);
-        };
-        for (var i = 0, len = arg_len; !self.halt && i < len; i++) {
-            if (null != args[i].call(self, chain, error)) {
-                chain();
-            }
-        }
-    });
-
-    //defer() - Defer execution of the next method
-    add('defer', function (args) {
-        var self = this;
-        setTimeout(function () {
-            self.next(true);
-        }, args.shift());
-    });
-
-    //onError() - Attach an error handler
-    add('onError', function (args, arg_len) {
-        var self = this;
-        this.error = function (err) {
-            self.halt = true;
-            for (var i = 0; i < arg_len; i++) {
-                args[i].call(self, err);
-            }
-        };
-    });
-
-}(Chain));
-
-    return Chain;
-});
-/*!
-
-Queue.js
-
-A function to represent a queue
-
-Created by Stephen Morley - http://code.stephenmorley.org/ - and released under
-the terms of the CC0 1.0 Universal legal code:
-
-http://creativecommons.org/publicdomain/zero/1.0/legalcode
-
-*/
-/*global define*/
-define('ThirdParty/Queue',[],function() {
-
-    /* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
-     * items are added to the end of the queue and removed from the front.
-     */
-    function Queue() {
-        // initialise the queue and offset
-        var queue = [];
-        var offset = 0;
-
-        /* Returns the length of the queue.
-         */
-        this.getLength = function() {
-            // return the length of the queue
-            return (queue.length - offset);
-        };
-
-        /* Returns true if the queue is empty, and false otherwise.
-         */
-        this.isEmpty = function() {
-            // return whether the queue is empty
-            return (queue.length === 0);
-        };
-
-        /* Enqueues the specified item. The parameter is:
-         *
-         * item - the item to enqueue
-         */
-        this.enqueue = function(item) {
-            // enqueue the item
-            queue.push(item);
-        };
-
-        /* Dequeues an item and returns it. If the queue is empty then undefined is
-         * returned.
-         */
-        this.dequeue = function() {
-            // if the queue is empty, return undefined
-            if (queue.length === 0) {
-                return undefined;
-            }
-
-            // store the item at the front of the queue
-            var item = queue[offset];
-
-            // increment the offset and remove the free space if necessary
-            if (++offset * 2 >= queue.length) {
-                queue = queue.slice(offset);
-                offset = 0;
-            }
-
-            // return the dequeued item
-            return item;
-        };
-
-        /* Returns the item at the front of the queue (without dequeuing it). If the
-         * queue is empty then undefined is returned.
-         */
-        this.peek = function() {
-            // return the item at the front of the queue
-            return (queue.length > 0 ? queue[offset] : undefined);
-        };
-    }
-
-    return Queue;
-});
+/*! Copyright (c) 2010 Chris O'Hara <cohara87@gmail.com>. MIT Licensed *//*global define*/define('ThirdParty/Chain',[],function() {    var Chain = {};(function(exports) {    exports = exports || {};    var handlers = {}, createChain, add;    createChain = function (context, stack, lastMethod) {        var inHandler = context.halt = false;        //The default error handler        context.error = function (e) {            throw e;        };        //Run the next method in the chain        context.next = function (exit) {            if (exit) {                inHandler = false;            }            if (!context.halt && stack && stack.length) {                var args = stack.shift(), method = args.shift();                inHandler = true;                try {                    handlers[method].apply(context, [args, args.length, method]);                } catch (e) {                    context.error(e);                }            }            return context;        };        //Bind each method to the context        for (var alias in handlers) {            if (typeof context[alias] === 'function') {                continue;            }            (function (alias) {                context[alias] = function () {                    var args = Array.prototype.slice.call(arguments);                    if (alias === 'onError') {                        if (stack) {                            handlers.onError.apply(context, [args, args.length]);                            return context;                        }                        var new_context = {};                        handlers.onError.apply(new_context, [args, args.length]);                        return createChain(new_context, null, 'onError');                    }                    args.unshift(alias);                    if (!stack) {                        return createChain({}, [args], alias);                    }                    context.then = context[alias];                    stack.push(args);                    return inHandler ? context : context.next();                };            }(alias));        }        //'then' is an alias for the last method that was called        if (lastMethod) {            context.then = context[lastMethod];        }        //Used to call run(), chain() or another existing method when defining a new method        //See load.js (https://github.com/chriso/load.js/blob/master/load.js) for an example        context.call = function (method, args) {            args.unshift(method);            stack.unshift(args);            context.next(true);        };        return context.next();    };    //Add a custom method/handler (see below)    add = exports.addMethod = function (method /*, alias1, alias2, ..., callback */) {        var args = Array.prototype.slice.call(arguments),            handler = args.pop();        for (var i = 0, len = args.length; i < len; i++) {            if (typeof args[i] === 'string') {                handlers[args[i]] = handler;            }        }        //When no aliases have been defined, automatically add 'then<Method>'        //e.g. adding 'run' also adds 'thenRun' as a method        if (!--len) {            handlers['then' + method.substr(0,1).toUpperCase() + method.substr(1)] = handler;        }        createChain(exports);    };    //chain() - Run each function sequentially    add('chain', function (args) {        var self = this, next = function () {            if (self.halt) {                return;            } else if (!args.length) {                return self.next(true);            }            try {                if (null != args.shift().call(self, next, self.error)) {                    next();                }            } catch (e) {                self.error(e);            }        };        next();    });    //run() - Run each function in parallel and progress once all functions are complete    add('run', function (args, arg_len) {        var self = this, chain = function () {            if (self.halt) {                return;            } else if (!--arg_len) {                self.next(true);            }        };        var error = function (e) {            self.error(e);        };        for (var i = 0, len = arg_len; !self.halt && i < len; i++) {            if (null != args[i].call(self, chain, error)) {                chain();            }        }    });    //defer() - Defer execution of the next method    add('defer', function (args) {        var self = this;        setTimeout(function () {            self.next(true);        }, args.shift());    });    //onError() - Attach an error handler    add('onError', function (args, arg_len) {        var self = this;        this.error = function (err) {            self.halt = true;            for (var i = 0; i < arg_len; i++) {                args[i].call(self, err);            }        };    });}(Chain));    return Chain;});
+/*!Queue.jsA function to represent a queueCreated by Stephen Morley - http://code.stephenmorley.org/ - and released underthe terms of the CC0 1.0 Universal legal code:http://creativecommons.org/publicdomain/zero/1.0/legalcode*//*global define*/define('ThirdParty/Queue',[],function() {    /* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -     * items are added to the end of the queue and removed from the front.     */    function Queue() {        // initialise the queue and offset        var queue = [];        var offset = 0;        /* Returns the length of the queue.         */        this.getLength = function() {            // return the length of the queue            return (queue.length - offset);        };        /* Returns true if the queue is empty, and false otherwise.         */        this.isEmpty = function() {            // return whether the queue is empty            return (queue.length === 0);        };        /* Enqueues the specified item. The parameter is:         *         * item - the item to enqueue         */        this.enqueue = function(item) {            // enqueue the item            queue.push(item);        };        /* Dequeues an item and returns it. If the queue is empty then undefined is         * returned.         */        this.dequeue = function() {            // if the queue is empty, return undefined            if (queue.length === 0) {                return undefined;            }            // store the item at the front of the queue            var item = queue[offset];            // increment the offset and remove the free space if necessary            if (++offset * 2 >= queue.length) {                queue = queue.slice(offset);                offset = 0;            }            // return the dequeued item            return item;        };        /* Returns the item at the front of the queue (without dequeuing it). If the         * queue is empty then undefined is returned.         */        this.peek = function() {            // return the item at the front of the queue            return (queue.length > 0 ? queue[offset] : undefined);        };    }    return Queue;});
 /*global define*/
 define('Core/PolygonPipeline',[
         './DeveloperError',
