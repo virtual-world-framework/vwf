107d2934f96e9826040756ae07d4d77cac269c99
David Easter <david.easter@lmco.com>
2012-09-24 02:44:03 +0000

    Expose the Alea generator state.
    git-svn-id: http://svn.virtualworldframework.com/vwf/branches/id@2544 775f9103-8eee-4f94-88be-65e0ca6d8b75

--- support/client/lib/alea.js
+++ support/client/lib/alea.js
@@ -2,40 +2,42 @@
 function Alea() {
   return (function(args) {
     // Johannes Baagøe <baagoe@baagoe.com>, 2010
-    var s0 = 0;
-    var s1 = 0;
-    var s2 = 0;
-    var c = 1;
+    var state = {
+      s0: 0,
+      s1: 0,
+      s2: 0,
+      c: 1
+    };
 
     if (args.length == 0) {
       args = [+new Date];
     }
     var mash = Mash();
-    s0 = mash(' ');
-    s1 = mash(' ');
-    s2 = mash(' ');
+    state.s0 = mash(' ');
+    state.s1 = mash(' ');
+    state.s2 = mash(' ');
 
     for (var i = 0; i < args.length; i++) {
-      s0 -= mash(args[i]);
-      if (s0 < 0) {
-        s0 += 1;
+      state.s0 -= mash(args[i]);
+      if (state.s0 < 0) {
+        state.s0 += 1;
       }
-      s1 -= mash(args[i]);
-      if (s1 < 0) {
-        s1 += 1;
+      state.s1 -= mash(args[i]);
+      if (state.s1 < 0) {
+        state.s1 += 1;
       }
-      s2 -= mash(args[i]);
-      if (s2 < 0) {
-        s2 += 1;
+      state.s2 -= mash(args[i]);
+      if (state.s2 < 0) {
+        state.s2 += 1;
       }
     }
     mash = null;
 
     var random = function() {
-      var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
-      s0 = s1;
-      s1 = s2;
-      return s2 = t - (c = t | 0);
+      var t = 2091639 * state.s0 + state.c * 2.3283064365386963e-10; // 2^-32
+      state.s0 = state.s1;
+      state.s1 = state.s2;
+      return state.s2 = t - (state.c = t | 0);
     };
     random.uint32 = function() {
       return random() * 0x100000000; // 2^32
@@ -46,6 +48,7 @@ function Alea() {
     };
     random.version = 'Alea 0.9';
     random.args = args;
+    random.state = state;
     return random;
 
   } (Array.prototype.slice.call(arguments)));
