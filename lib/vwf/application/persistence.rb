a78d460a2aa49eeca0dae2409509129792a54b26
Allison Corey <allison.a.corey@lmco.com>
2013-01-24 14:55:14 -0500

    Change how state is written to file to remove POST errors for long states.

--- lib/vwf/application/persistence.rb
+++ lib/vwf/application/persistence.rb
@@ -26,8 +26,13 @@ class VWF::Application::Persistence < Sinatra::Base
 
     pass unless settings.development? # only in development mode
 
-    File.open('public/'+params["root"]+'/'+params["filename"]+'.vwf.json', 'w') {|f| f.write(params["jsonState"]) }
-    File.open('public/'+params["root"]+'/'+params["filename"]+'_'+params["inst"]+'_'+params["timestamp"]+'.vwf.json', 'w') {|f| f.write(params["jsonState"]) }
+    f = File.open('public/'+params["root"]+'/'+params["filename"]+'.vwf.json', 'w')
+    f.puts params["jsonState"]
+    f.close
+
+    f = File.open('public/'+params["root"]+'/'+params["filename"]+'_'+params["inst"]+'_'+params["timestamp"]+'.vwf.json', 'w')
+    f.puts params["jsonState"]
+    f.close
 
   end
 
