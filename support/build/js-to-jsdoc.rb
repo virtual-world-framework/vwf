061467fadc59ce2fc2c9027963c6cc7ceb41f009
David Easter <david.easter@lmco.com>
2013-01-28 13:08:53 -0500

    Faster and simpler JSDoc comment conversions.
    - Move the /// and ## to /** ... */ conversions into external scripts,
      and convert all files at once (much faster than starting ruby once per
      file).
    
    - Remove the "/** namespace */ function module.name() ..." hack from the
      conversion in anticipation of switching to JSDoc 3.
    
    - Invoke JSDoc as: sh "java", "-jar", ... instead of: sh "java -jar ..."
      to bypass the shell (avoids problems with meta characters in file
      names.)

---
+++ support/build/js-to-jsdoc.rb
@@ -0,0 +1,18 @@
+# Convert "///" comment blocks to JSDoc-style /** ... */ blocks. Invoke as:
+# 
+#   ruby -p -i js-to-jsdoc.rb file ...
+# 
+# to convert a set of files in place.
+
+case ( state ||= 0 ) + ( %r{^\s*///}.match( $_ ) ? 1 : 0 )
+    when 1  # entering block
+        last = $_
+        sub %r{///}, "/**"
+        state = 2
+    when 3  # in block
+        last = $_
+        sub %r{///}, " * "
+    when 2  # leaving block
+        puts last.sub %r{///.*}, " */"
+        state = 0
+end
