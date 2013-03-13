40a3800995e0a905b7d6ee39636e1b7b3efa9855
Scott Haynes <scott.r.haynes@lmco.com>
2013-03-12 23:55:22 -0400

    Use the xml collada element name before using the id if defined

--- support/client/lib/vwf/model/threejs/js/loaders/ColladaLoader.js
+++ support/client/lib/vwf/model/threejs/js/loaders/ColladaLoader.js
@@ -841,7 +841,7 @@ THREE.ColladaLoader = function () {
 
 		}
 
-		obj.name = node.id || "";
+		obj.name = node.name || node.id || "";
 		obj.matrix = node.matrix;
 		var props = node.matrix.decompose();
 		obj.position = props[ 0 ];
