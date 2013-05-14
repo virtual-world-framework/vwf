284db6b16ae409f178848606bf5d4c1bee84553e
Mick Muzac <mmuzac23@gmail.com>
2013-05-14 16:18:12 -0400

    Major UI improvements, minor routing changes, bug fixes

--- landingRoutes.js
+++ landingRoutes.js
@@ -2,42 +2,41 @@ var root = '/adl/sandbox';
 
 exports.index = function(req, res){
 
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = {root: root};
 	res.render('index');
 }
 exports.create = function(req, res){
 
 
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = {root: root};
 	res.render('create');
 }
 exports.signup = function(req, res){
 
 
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = {root: root};
 	res.render('signup');
 }
 exports.login = function(req, res){
 
 
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = {root: root};
 	res.render('login');
 }
 exports.logout = function(req, res){
 
 
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = {root: root};
 	res.render('logout');
 }
 exports.edit = function(req, res){
 
-
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = { sid: root + '/' + (req.query.id?req.query.id:'') + '/', root: root};
 	res.render('editInstance');
 }
 exports.remove = function(req, res){
 
 
-	res.locals = { 'test' : 'Hey now.', root: root};
+	res.locals = { sid: root + '/' + (req.query.id?req.query.id:'') + '/', root: root};
 	res.render('deleteInstance');
 }
