435127e307b7eb1a5c30d798665350f45ce06581
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2013-06-17 11:13:00 -0400

    Filled out plumbing for RTC objects

--- support/client/lib/vwf/view/WebRTC.js
+++ support/client/lib/vwf/view/WebRTC.js
@@ -1,63 +1,57 @@
-define( [ "module", "vwf/view" ], function( module, view ) {
+/*
+ * WebRTC.js : Behaves as a wrapper for vwf/rtcObject
+ * Maps simple 1:1 signal model to a broadcast model using target and sender ids
+ */
 
-    // vwf/model/object.js is a backstop property store.
+define( [ "module", "vwf/view", "vwf/rtcObject" ], function( module, view, RTCObject ) {
 
-    return view.load( module, {
-	
-		calledMethod : function(id,name,params){
-			
-			
+	return view.load( module, {
 
-			if(id == 'index-vwf' && name == 'createRTCCall')
+		initialize : function()
+		{
+			// create a new rtc object on view initialization
+			this.rtc = new RTCObject(null,null,this.send.bind(this),null);
+		},
+
+
+		/*
+		 * Receives incoming messages
+		 */
+		calledMethod : function(id,name,params)
+		{
+			if( id != 'index-vwf' )
+				return;
+
+			if( name == 'rtcCall' )
 			{
-				debugger;
-				if(!this.rtc)
-					this.createRTCclient(params);
-			}  
-			//which will end up here
-			if(id == 'index-vwf' && name == 'rtcAnswer')
+				this.rtcTarget = params.target;
+				this.rtc.initialize({'video':true, 'audio':true});
+			}
+
+			else if( name == 'rtcData' )
 			{
-				
-				if(_UserManager.getCurrentUserName() == params.target)
+				if(_UserManager.GetCurrentUserName() == params.target)
 				{
-					if(this.rtcTraget == null) this.rtcTraget = params.sender;
-					if(!this.rtc)
-					   this.createRTCclient(params);
+					if(this.rtcTraget == null)
+						this.rtcTraget = params.sender;
+
+					if( this.rtc.peerConn == null )
+						this.rtc.initialize({'video':true, 'audio':true});
 					else
-						rtc.receiveCallback(params.rtcData);        
+						this.rtc.receiveCallback(params.rtcData);
 				}
 			}
 		},
 		
-		initialize : function(id)
-		{
-			this.rtc = null;
-			if(id == 'index-vwf')
-			{
-				alert('goit here');
-			}
-		},
 
-		createRTCclient : function(params)
-		{
-		   this.rtcTarget = params;
-		   var name = params;
-		   if(this.rtc)
-			   this.rtc.disconnect();
-		   this.rtc = new rtc(null,null,
-			   this.send.bind(this),    
-			   null);
-		   this.rtc.initialize({video: true, audio: true});    
-		},
+		// send a signal to the other clients
 		send : function(data) 
 		{
-			var payload = [];
-			payload.rtcData= data;
+			var payload = {};
+			payload.rtcData = data;
 			payload.target = this.rtcTarget;
-			payload.sender = _UserManager.getCurrentUserName();
-			//this actuall sends a packtet
-			vwf_view.callMethod('index-vwf', 'rtcAnswer', payload); //send
-
+			payload.sender = _UserManager.GetCurrentUserName();
+			vwf_view.kernel.callMethod('index-vwf', 'rtcData', payload); //send
 		}
 	
 	})
