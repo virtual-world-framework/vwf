virtual-world-framework/vwf

commit aa0849bf48faa8df1af6f61e7d9d6f4044bd0356

Add error checking to catch my silly coding mistakes
refs #1880

support/client/lib/vwf.js
@@ -3128,6 +3128,10 @@ if ( vwf.execute( childID, "Boolean( this.tick )" ) ) {
         /// @see {@link module:vwf/api/kernel.children}
 
         this.children = function( nodeID ) {
+            if ( !nodeID ) {
+                this.logger.error( "Cannot retrieve children of nonexistent node" );
+                return;
+            }
             return this.models.object.children( nodeID );
         };
 
@@ -3138,6 +3142,11 @@ if ( vwf.execute( childID, "Boolean( this.tick )" ) ) {
         /// @see {@link module:vwf/api/kernel.descendants}
 
         this.descendants = function( nodeID ) {
+
+            if ( !nodeID ) {
+                this.logger.error( "Cannot retrieve children of nonexistent node" );
+                return;
+            }
 
             var descendants = [];
 

support/client/lib/vwf/model/object.js
@@ -302,9 +302,17 @@ if ( ! object ) return;  // TODO: patch until full-graph sync is working; driver
         // -- children -----------------------------------------------------------------------------
 
         children: function( nodeID ) {
-            return this.objects[nodeID].children.map( function( child ) {
-                return child.id;
-            } );
+            if ( !nodeID ) {
+                this.logger.error( "Cannot retrieve children of nonexistent node");
+                return;
+            }
+            var node = this.objects[ nodeID ];
+            if ( node )
+                return node.children.map( function( child ) {
+                    return child.id;
+                } );
+            else
+                this.logger.error( "Cannot find node: " + nodeID );
         },
 
         // == Special utilities ====================================================================

commit 680de6b1be9c08f5d807d1d77da081813a43d664

Fix test/replication.html, broken in commit:aa0849b.

support/client/lib/vwf.js
@@ -3128,10 +3128,12 @@ if ( vwf.execute( childID, "Boolean( this.tick )" ) ) {
         /// @see {@link module:vwf/api/kernel.children}
 
         this.children = function( nodeID ) {
-            if ( !nodeID ) {
-                this.logger.errorx( "Cannot retrieve children of nonexistent node" );
+
+            if ( nodeID === undefined ) {
+                this.logger.errorx( "children", "cannot retrieve children of nonexistent node" );
                 return;
             }
+
             return this.models.object.children( nodeID );
         };
 
@@ -3143,8 +3145,8 @@ if ( vwf.execute( childID, "Boolean( this.tick )" ) ) {
 
         this.descendants = function( nodeID ) {
 
-            if ( !nodeID ) {
-                this.logger.errorx( "Cannot retrieve children of nonexistent node" );
+            if ( nodeID === undefined ) {
+                this.logger.errorx( "descendants", "cannot retrieve children of nonexistent node" );
                 return;
             }
 

support/client/lib/vwf/model/object.js
@@ -316,17 +316,21 @@ if ( ! object ) return;  // TODO: patch until full-graph sync is working; driver
         // -- children -----------------------------------------------------------------------------
 
         children: function( nodeID ) {
-            if ( !nodeID ) {
-                this.logger.errorx( "Cannot retrieve children of nonexistent node");
+
+            if ( nodeID === undefined ) {
+                this.logger.errorx( "children", "cannot retrieve children of nonexistent node");
                 return;
             }
+
             var node = this.objects[ nodeID ];
+
             if ( node )
                 return node.children.map( function( child ) {
                     return child.id;
                 } );
             else
                 this.logger.error( "Cannot find node: " + nodeID );
+
         },
 
         // == Special utilities ====================================================================