dd1865eeb10ab0e3ef7d2dbf7a13f5c8f4e3fc32
rchadwic <rchadwic@gmail.com>
2013-08-21 17:22:47 -0400

    first support for sounds!

--- support/client/lib/vwf/view/audio.js
+++ support/client/lib/vwf/view/audio.js
@@ -11,13 +11,33 @@ define( [ "module", "vwf/view", "vwf/view/buzz/buzz.min"], function( module, vie
 		initialize : function()
 		{
 			this.buzz = require("buzz");
-			
+			this.sounds = {};
 		},
-
 		calledMethod : function(id,name,params)
 		{
-			
+			if(name == 'playSound')
+			{
+				
+				var url = params[0];
+				var loop = params[1] || false;
+				
+				if(this.sounds[url])
+				{
+					this.sounds[url].play();
+					if(loop)
+					this.sounds[url].loop();
+					else
+					this.sounds[url].unloop();
+				}
+				else
+				{
+					var mySound = new this.buzz.sound(url,{
+						autoplay: true,
+						loop: loop
+					});
+					this.sounds[url] = mySound;
+				}
+			}
 		}
-	
 	})
 });
