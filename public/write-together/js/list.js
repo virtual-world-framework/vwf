0fc34d2f847097998f3f98d63665d4184383744a
Jess Martin <jessmartin@gmail.com>
2013-08-26 11:30:53 -0400

    Extracting the JS to a separate file

---
+++ public/write-together/js/list.js
@@ -0,0 +1,9 @@
+$(function(){
+  $.getJSON('listallsaves', function(data) {
+    $.each(data, function(key, value) {
+      if (value['latestsave']) {
+        $('#documents').append("<li><a href='/write-together/load/" + value['savename'] + "/" + value['revision'] + "'>" + value['savename'] + "</a></li>");
+      }
+    });
+  });
+});
