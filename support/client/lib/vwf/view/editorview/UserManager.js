059b33cfd6cfc78b757e70563b282d6aa418e28e
Mick Muzac <mmuzac23@gmail.com>
2013-08-28 13:08:29 -0400

    Both in-world log in buttons redirect with hashtag

--- support/client/lib/vwf/view/editorview/UserManager.js
+++ support/client/lib/vwf/view/editorview/UserManager.js
@@ -151,7 +151,7 @@ define(function ()
 								$(document.body).append('<a href="#" id="GuestLogin" style="font-family: sans-serif;z-index:99;position:fixed;font-size: 2em;" class="alertify-button alertify-button-ok" id="alertify-ok">Login</a>');
 								$('#GuestLogin').click(function()
 								{
-									window.location = _DataManager.getCurrentApplication() + "/login?return=" + _DataManager.getCurrentSession().substr(13);
+									window.location = _DataManager.getCurrentApplication() + "/login?return=" + _DataManager.getCurrentSession().substr(13) + window.location.hash;
 								});
 							}
 						}
@@ -413,7 +413,7 @@ define(function ()
 						
 						window.onbeforeunload = '';
 						$(window).unbind();
-						window.location = _DataManager.getCurrentApplication() + "/login?return=" + _DataManager.getCurrentSession().substr(13);
+						window.location = _DataManager.getCurrentApplication() + "/login?return=" + _DataManager.getCurrentSession().substr(13) + window.location.hash;
 					}.bind(this)
 				});
 			
