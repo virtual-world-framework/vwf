165a1c914fcd5a9e60d5b33c739980b00b2a82cd
Jess Martin <jessmartin@gmail.com>
2013-12-06 13:00:04 -0600

    Fix for safari.

--- support/client/lib/vwf/view/webrtc/adapter.js
+++ support/client/lib/vwf/view/webrtc/adapter.js
@@ -10,7 +10,7 @@ function trace(text) {
   if (text[text.length - 1] == '\n') {
     text = text.substring(0, text.length - 1);
   }
-  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+  // console.log((performance.now() / 1000).toFixed(3) + ": " + text);
 }
 
 console.log( navigator.userAgent );
