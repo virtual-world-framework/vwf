ea5b7f1ca71a67af16b01edbcb91a7e11c4d6a65
Jess Martin <jessmartin@gmail.com>
2013-12-10 15:35:00 -0600

    Implement `vwf create` and add mocha tests.
    Refs #2962

--- node-server.js
+++ node-server.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 var server = require( './node_vwf' ),
-    fs 	   = require( 'fs' );
+    fs 	   = require( 'fs' ),
+    cli    = require( './lib/nodejs/vwfCli.js' );
 
 var argv = require( 'optimist' ).argv;
 
@@ -24,11 +25,7 @@ function printGeneralHelp() {
 	console.log("  -h, --help               Output usage information");
 }
 
-if ( argv._.indexOf( 'create' ) == 0 ) {
-	console.log( "Creating a new application" );
-} else if ( argv.help || argv.h || ( argv._[0] == 'help' && argv._.length == 1 ) ) {
-	printGeneralHelp();
-} else if ( argv._[0] == 'help' && argv._[1] == 'create' ) {
+function printCreateHelp() {
 	console.log("Usage: vwf create APP_PATH");
 	console.log("");
 	console.log("The `vwf create` command creates a new VWF application with a");
@@ -36,6 +33,31 @@ if ( argv._.indexOf( 'create' ) == 0 ) {
 	console.log("");
 	console.log("Example:");
 	console.log("  vwf create ~/code/my-new-app");
+}
+
+if ( argv._[0] == 'create' && argv._.length == 1 ) {
+	console.log("'create' requires a PATH to create the new VWF application.");
+	console.log("");
+	printCreateHelp();	
+} else if ( argv._[0] == 'create' && argv._.length > 1 ) {
+	var applicationPath = argv._[1];
+
+	if ( cli.create( applicationPath ) ) {
+		console.log("VWF application created at '" + applicationPath + "'.");
+		console.log("");
+		console.log("To get started quickly:");
+		console.log("  $ cd " + applicationPath);
+		console.log("  $ vwf");
+		console.log("");
+		console.log("See the Getting Started documentation at: ");
+		console.log("http://www.virtual.wf/web/docs/readme.html");
+	} else {
+		console.log("VWF application could not be created at '" + applicationPath + "'");
+	}
+} else if ( argv.help || argv.h || ( argv._[0] == 'help' && argv._.length == 1 ) ) {
+	printGeneralHelp();
+} else if ( argv._[0] == 'help' && argv._[1] == 'create' ) {
+	printCreateHelp();
 } else if ( argv._[0] == 'help' && argv._[1] == 'run' ) {
 	console.log("Usage:");
 	console.log("  vwf [options]");
