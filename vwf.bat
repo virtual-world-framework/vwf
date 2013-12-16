79d531e12935bf07a215e59883a9f60b0789bb74
Michael Vacirca <michael.j.vacirca@lmco.com>
2013-12-16 16:25:17 -0500

    Remove the forced reboot by updating path for the current command prompt session.

--- vwf.bat
+++ vwf.bat
@@ -19,20 +19,19 @@ goto :start
 path=%path%;C:\Program Files (x86)\nodejs\;C:\Program Files\nodejs\
 echo "VWF_DIR" variable is not set. 
 echo This occurs the first time you execute VWF or if you have moved your VWF folder.  
-echo To complete your installation please enter your directory path for VWF. 
 echo A restart will be required to complete configuration.
-set /p VWF_DIR="Enter VWF Folder Location: "
+VWF_DIR==%CD%
 REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH /f 
 REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v VWF_DIR /f 
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v VWF_DIR /d !VWF_DIR!
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH /d "%path%;!VWF_DIR!"
-echo "VWF_DIR" is now set to !VWF_DIR!. You will need to restart your computer now.
-echo Shutting down in 30 seconds. Please type "shutdown /a" to abort.
-cmd.exe /K shutdown /f /t 30 /r
+path=%path%;!VWF_DIR!
+echo "VWF_DIR" is now set to !VWF_DIR!. You will need to restart your computer to start using vwf from the command prompt.
+
 
 :start
 REM Check to see if Node is installed and accessible from the Path variable
 REM  If it is, we start up the NodeJS Server. The NodeJS server accepts command line parameters.
 REM  C:\> vwf --help will display all of the currently available parameters accepted by Virtual World Framework
 pushd %VWF_DIR% && cmd.exe /C npm install && popd && cmd.exe /C node "%VWF_DIR%/node-server.js" %*
-
+endlocal
