2c2be9b6a81a9aefcf44714821ba29b8a444c08b
David Easter <david.easter@lmco.com>
2014-02-13 21:04:01 -0500

    Refactor `kernel.prototypes` slightly to remove duplicated code.

--- support/client/lib/vwf.js
+++ support/client/lib/vwf.js
@@ -3488,27 +3488,23 @@ if ( ! childComponent.source ) {
 
             var prototypes = [];
 
-            if ( includeBehaviors ) {
-                var b = [].concat( this.behaviors( nodeID ) );
-                Array.prototype.push.apply( prototypes, b.reverse() );
-            }
+            do {
 
-            nodeID = this.prototype( nodeID );
-
-            while ( nodeID ) {
-                
-                // Add the current node in the prototype chain
-                prototypes.push( nodeID );
-
-                // Add the current node's behaviors
+                // Add the current node's behaviors.
                 if ( includeBehaviors ) {
                     var b = [].concat( this.behaviors( nodeID ) );
                     Array.prototype.push.apply( prototypes, b.reverse() );
                 }
 
-                // Get the next node in the prototype chain
+                // Get the next prototype.
                 nodeID = this.prototype( nodeID );
-            }
+
+                // Add the prototype.
+                if ( nodeID ) {
+                    prototypes.push( nodeID );
+                }
+
+            } while ( nodeID );
 
             return prototypes;
         };
