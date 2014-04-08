4b085054e22b315427baeccb454f056cde17386f
Scott Haynes <scottnc27603@gmail.com>
2014-04-08 12:30:54 -0400

    Updates the blockly demo to use the sphere and cube from threes

--- public/blockly-node3/blocks_object.js
+++ public/blockly-node3/blocks_object.js
@@ -1,23 +1,29 @@
 
+// node3_objects =====================================
 
 Blockly.Blocks['node3_objects'] = {
   init: function() {
     this.setHelpUrl('http://www.example.com/');
-    this.setColour(359);
-    this.appendValueInput("NODE3NAME")
-        .appendField("node3Name");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["redCube", "REDCUBE"], ["greenSphere", "GREENSPHERE"], ["blueCube", "BLUECUBE"], ["blueSphere", "BLUESPHERE"]]), "NODE3_NAME");
+    this.appendStatementInput("NODE3_STATEMENTS");
+    this.setInputsInline(true);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setTooltip('');
   }
 };
 
-
-
-
 Blockly.JavaScript['node3_objects'] = function(block) {
-  var value_node3name = Blockly.JavaScript.valueToCode(block, 'NODE3NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_name = Blockly.JavaScript.statementToCode(block, 'NODE3_STATEMENTS');
+  var dropdown_node3_name = block.getFieldValue('NODE3_NAME');
+  var code = '';
+  var id = Blockly.Blocks.vwfNodes[ dropdown_node3_name ];
+  if ( id !== undefined ) {
+
+  }
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
   return code;
 };
+
+
