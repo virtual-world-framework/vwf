e917ac7972242de736b8791a00d8140b05827cae
Eric Burns <eric.burns@lmco.com>
2014-04-09 10:52:09 -0400

    Correct documentation comment in view/blockly.js

--- support/client/lib/vwf/view/blockly.js
+++ support/client/lib/vwf/view/blockly.js
@@ -11,7 +11,7 @@
 // or implied. See the License for the specific language governing permissions and limitations under
 // the License.
 
-/// @module vwf/view/test
+/// @module vwf/view/blockly
 /// @requires vwf/view
 
 define( [ "module", "vwf/view" ], function( module, view ) {
