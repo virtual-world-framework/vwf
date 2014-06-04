5e82864ced758c568588833969f281f99cdb1549
Brett Swift <brett.a.swift@knights.ucf.edu>
2014-06-04 13:03:58 -0400

    Fixed conditional

--- support/client/lib/vwf/model/threejs/js/loaders/ColladaLoader.js
+++ support/client/lib/vwf/model/threejs/js/loaders/ColladaLoader.js
@@ -3556,7 +3556,7 @@ THREE.ColladaLoader = function () {
 
 								props[ 'emissive' ] = cot.color.getHex();
 
-							} if ( prop === 'specularLevel' ) {
+							} else if ( prop === 'specularLevel' ) {
 
 								props[ 'specular' ] = cot.color.getHex();
 
