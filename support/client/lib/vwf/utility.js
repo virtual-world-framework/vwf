685efe0fe40cddd03ebec93ce91532c81f9f71f3
Scott Haynes <scottnc27603@gmail.com>
2014-06-30 17:05:32 -0400

    Reverts changes from previous commit

--- support/client/lib/vwf/utility.js
+++ support/client/lib/vwf/utility.js
@@ -20,13 +20,11 @@
 define( [ "module",
     "vwf/utility/xpath",
     "vwf/utility/color",
-    "vwf/utility/coordinates",
-    "vwf/utility/cws"
+    "vwf/utility/coordinates"
 ], function( module,
     xpath,
     color,
-    coordinates,
-    cws
+    coordinates
 ) {
 
     var exports = {
@@ -292,12 +290,6 @@ define( [ "module",
         /// DOM element coordinate conversion functions.
 
         coordinates: coordinates,
-
-        // -- cws --------------------------------------------------------------------------
-
-        /// MIL-STD-2525B COMMON WARFIGHTING SYMBOLOGY
-
-        cws: cws,        
 
     };
 
