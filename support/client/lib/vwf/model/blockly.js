3c6b46365782df15c6e4872bf50b6513b63a0a83
Spencer Frazier <spencer.j.frazier@lmco.com>
2014-07-02 12:55:52 -0400

    Fixed fencepost error in blockly model causing parameters not to pass in vwf.callMethod

--- support/client/lib/vwf/model/blockly.js
+++ support/client/lib/vwf/model/blockly.js
@@ -533,7 +533,7 @@ define( [ "module", "vwf/model",
                                 if ( arguments[ j ].type === 'object' ) {
                                     var temp = [];
                                     if ( arguments[ j ].properties !== undefined ) {
-                                        for ( var k = 0; k <= arguments[ j ].properties.length; k++ ) {
+                                        for ( var k = 0; k < arguments[ j ].properties.length; k++ ) {
                                             temp.push( arguments[ j ].properties[ k ].toString() )
                                         }
                                     }
