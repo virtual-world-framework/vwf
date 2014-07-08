0b957c531b35d1f6ea26196c1395bc89aec98d44
Spencer Frazier <spencer.j.frazier@lmco.com>
2014-07-08 10:20:06 -0400

    Adds in the blockly trashcan.

--- support/client/lib/vwf/view/blockly.js
+++ support/client/lib/vwf/view/blockly.js
@@ -100,7 +100,8 @@ define( [ "module", "vwf/view", "jquery", "vwf/model/blockly/JS-Interpreter/acor
                 self.kernel.setProperty( childID, "toolbox", self.options.toolbox );
                 Blockly.inject( document.getElementById( self.options.divName ), { 
                     path: this.options.blocklyPath,
-                    toolbox: document.getElementById( self.options.toolbox ) 
+                    toolbox: document.getElementById( self.options.toolbox ),
+                    trashcan: true,
                 } ); 
 
                 Blockly.addChangeListener( function( event ) {
