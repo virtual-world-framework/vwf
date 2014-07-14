25549c0cd1a3abc72f1ec981055071ee266418cb
Scott Haynes <scottnc27603@gmail.com>
2014-07-14 08:40:58 -0400

    Updates Cesium to the latest version b30

---
+++ support/client/lib/vwf/model/cesium/Scene/PrimitiveState.js
@@ -0,0 +1,19 @@
+/*global define*/
+define(function() {
+    "use strict";
+
+    /**
+     * @private
+     */
+    var PrimitiveState = {
+        READY : 0,
+        CREATING : 1,
+        CREATED : 2,
+        COMBINING : 3,
+        COMBINED : 4,
+        COMPLETE : 5,
+        FAILED : 6
+    };
+
+    return PrimitiveState;
+});
