e947ba3da44f3225388947cd6c70f56b27dde83e
Kevin Dill <kevin.dill@lmco.com>
2014-07-16 12:07:21 -0400

    - Make the delay for queued items be a tuning parameter - Set the default delay to 0.8 seconds (instead of 0.4) - Add the sound name to a debug message

--- support/client/lib/vwf/model/sound.js
+++ support/client/lib/vwf/model/sound.js
@@ -387,6 +387,7 @@ define( [ "module", "vwf/model" ], function( module, model ) {
 
         soundGroup: undefined,
         groupReplacementMethod: undefined,
+        queueDelayTime: 0.8,  // in seconds
 
         // a counter for creating instance IDs
         instanceIDCounter: 0,
@@ -429,6 +430,10 @@ define( [ "module", "vwf/model" ], function( module, model ) {
             }
 
             this.groupReplacementMethod = this.soundDefinition.groupReplacementMethod;
+
+            if ( this.soundDefinition.queueDelayTime !== undefined ) {
+                this.queueDelayTime = this.soundDefinition.queueDelayTime;
+            }
 
             if ( this.groupReplacementMethod && !this.soundGroup ) {
                 logger.warnx( "soundDatum.initialize", 
@@ -567,7 +572,7 @@ define( [ "module", "vwf/model" ], function( module, model ) {
                     if ( !!nextInstance ) {
                         setTimeout( function() {
                             startSoundInstance( nextInstance );
-                        }, 400 );
+                        }, nextInstance.soundDatum.queueDelayTime * 1000 );
                     }
                 }
 
@@ -596,7 +601,8 @@ define( [ "module", "vwf/model" ], function( module, model ) {
 
                     default:
                         logger.errorx( "PlayingInstance.initialize",
-                                       "This sound is in a group, but doesn't " +
+                                       "This sound ('" + thisInstance.soundDatum.name + 
+                                       "') is in a group, but doesn't " +
                                        "have a valid replacement method!" );
 
                         stopSoundGroup( group );
