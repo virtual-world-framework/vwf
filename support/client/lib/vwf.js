24f08c1eda8d715408a66abd8a5f87e8bc1c8559
David Easter <david.easter@lmco.com>
2014-08-04 01:55:37 -0400

    Propagate registry collection changes to parent collections.

--- support/client/lib/vwf.js
+++ support/client/lib/vwf.js
@@ -5484,6 +5484,10 @@ if ( ! childComponent.source ) {
                             this.changes[ name ] = "changed";  // previously removed, then added
                         }
 
+                        if ( this.container && this.containerMember ) {
+                            this.container.change( this.containerMember );
+                        }
+
                     }
 
                     return true;
@@ -5517,6 +5521,10 @@ if ( ! childComponent.source ) {
                             delete this.changes[ name ];  // previously added, then removed
                         }
 
+                        if ( this.container && this.containerMember ) {
+                            this.container.change( this.containerMember );
+                        }
+
                     }
 
                     return true;
@@ -5543,6 +5551,10 @@ if ( ! childComponent.source ) {
                     if ( this.changes[ name ] !== "added" ) {
                         this.changes[ name ] = value ?
                             value : this.changes[ name ] || "changed";
+                    }
+
+                    if ( this.container && this.containerMember ) {
+                        this.container.change( this.containerMember );
                     }
 
                     return true;
@@ -5647,6 +5659,23 @@ if ( ! childComponent.source ) {
                 // ...
             },
 
+            /// The parent collection if this collection is a member of another. Changes applied to
+            /// members of this collection will call `container.change( containerMember )` to also
+            /// set the change flag for the containing member.
+            /// 
+            /// For example, members of the `node.events` collection contain listener collections at
+            /// `node.events.existing[name].listeners`. Each listener collection knows its event
+            /// name and points back to `node.events`. Changing a listener will call
+            /// `node.events.change( name )` to mark the event as changed.
+
+            container: undefined,
+
+            /// This collection's name in the parent if this collection is a member of another
+            /// collection. Changes to members of this collection will mark that member changed in
+            /// the containing collection.
+
+            containerMember: undefined,
+
         };
 
         /// Prototype for index-based, ordered collections in the node registry, including
@@ -5689,6 +5718,10 @@ if ( ! childComponent.source ) {
                             this.changes.changed.push( id );
                         }
 
+                        if ( this.container && this.containerMember ) {
+                            this.container.change( this.containerMember );
+                        }
+
                     }
 
                     return true;
@@ -5726,6 +5759,10 @@ if ( ! childComponent.source ) {
                             this.changes.added.splice( addedIndex, 1 );
                         }
 
+                        if ( this.container && this.containerMember ) {
+                            this.container.change( this.containerMember );
+                        }
+
                     }
 
                     return true;
@@ -5758,6 +5795,10 @@ if ( ! childComponent.source ) {
                     if ( addedIndex < 0 && changedIndex < 0 ) {
                         this.changes.changed = this.changes.changed || [];
                         this.changes.changed.push( id );
+                    }
+
+                    if ( this.container && this.containerMember ) {
+                        this.container.change( this.containerMember );
                     }
 
                     return true;
@@ -5853,6 +5894,23 @@ if ( ! childComponent.source ) {
                 // removed: [ id, ... ],
                 // changed: [ id, ... ],
             },
+
+            /// The parent collection if this collection is a member of another. Changes applied to
+            /// members of this collection will call `container.change( containerMember )` to also
+            /// set the change flag for the containing member.
+            /// 
+            /// For example, members of the `node.events` collection contain listener collections at
+            /// `node.events.existing[name].listeners`. Each listener collection knows its event
+            /// name and points back to `node.events`. Changing a listener will call
+            /// `node.events.change( name )` to mark the event as changed.
+
+            container: undefined,
+
+            /// This collection's name in the parent if this collection is a member of another
+            /// collection. Changes to members of this collection will mark that member changed in
+            /// the containing collection.
+
+            containerMember: undefined,
 
         };
 
