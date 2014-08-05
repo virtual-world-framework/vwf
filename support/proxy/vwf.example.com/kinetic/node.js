8a3f90881bb80793419f89aa49d493c2c57e3e4d
Scott Haynes <scottnc27603@gmail.com>
2014-08-05 12:18:35 -0400

    Adds a method for toggling visibility since one property value is 'inherit'

--- support/proxy/vwf.example.com/kinetic/node.js
+++ support/proxy/vwf.example.com/kinetic/node.js
@@ -1,33 +1,39 @@
 this.initialize = function() {
 	this.pointerIsDown = false;
-	this.lastPointerLocation = null;
+	this.previousVisible = undefined;
 }
 
+this.toggleVisibilty = function() {
+
+	var viz = this.visible;
+
+	// isVisible will take care of 'inherit', and 
+	// trace up through the scene graph to determine
+	// if the current state is visible or not
+	if ( this.isVisible ) {
+		this.visible = this.previousVisible ? this.previousVisible : false;
+	} else {
+		this.visible = this.previousVisible ? this.previousVisible : true;	
+	}
+
+	if ( viz === 'inherit' ) {
+		this.previousVisible = 'inherit';
+	} else {
+		this.previousVisible = undefined;
+	} 
+
+}
+
+
 this.pointerDown = function( eventData, nodeData ) {
-
 	this.pointerIsDown = true;
-	this.lastPointerLocation = eventData.location;
-
 }
 
 this.pointerMove = function( eventData, nodeData ) {
 
 	if ( this.draggable && this.pointerIsDown ) {
-		// if ( this.client !== this.moniker ) {
-		// 	var loc = [ this.x, this.y ];
-
-		// 	console.info( "dx: " + ( eventData.location[ 0 ] - this.lastPointerLocation[ 0 ] ) ); 
-		// 	console.info( "dy: " + ( eventData.location[ 1 ] - this.lastPointerLocation[ 1 ] ) );
-
-		// 	loc[ 0 ] += eventData.location[ 0 ] - this.lastPointerLocation[ 0 ];
-		// 	loc[ 1 ] += eventData.location[ 1 ] - this.lastPointerLocation[ 1 ];
-		// 	this.x = loc[ 0 ];
-		// 	this.y = loc[ 1 ];
-		// 	this.lastPointerLocation = eventData.location;
-		// }
 		if ( this.client === this.moniker ) {
-			vwf_view.kernel.setProperty( this.id, "x", this.x );
-			vwf_view.kernel.setProperty( this.id, "y", this.y );
+			vwf_view.kernel.setProperty( this.id, "position", [ this.x, this.y ] );
 		}
 
 	}
@@ -36,19 +42,10 @@ this.pointerMove = function( eventData, nodeData ) {
 this.pointerUp = function( eventData, nodeData ) {
 
 	if ( this.draggable ) {
-		// if ( this.client !== this.moniker ) {
-		// 	var loc = [ this.x, this.y ];
-		// 	loc[ 0 ] += eventData.location[ 0 ] - this.lastPointerLocation[ 0 ];
-		// 	loc[ 1 ] += eventData.location[ 1 ] - this.lastPointerLocation[ 1 ];
-		// 	this.x = loc[ 0 ];
-		// 	this.y = loc[ 1 ];
-		// }
 		if ( this.client === this.moniker ) {
-			vwf_view.kernel.setProperty( this.id, "x", this.x );
-			vwf_view.kernel.setProperty( this.id, "y", this.y );
+			vwf_view.kernel.setProperty( this.id, "position", [ this.x, this.y ] );
 		}
 	}
 	
 	this.pointerIsDown = false;
-	this.lastPointerLocation = null;
 }  //@ sourceURL=kinetic_node.js
