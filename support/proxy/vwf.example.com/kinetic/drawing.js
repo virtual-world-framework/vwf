d74452ed52c2cfb44c993216e7b4f7e41b0ca23d
Scott Haynes <scottnc27603@gmail.com>
2014-08-06 15:34:18 -0400

    Corrects per user drawing

--- support/proxy/vwf.example.com/kinetic/drawing.js
+++ support/proxy/vwf.example.com/kinetic/drawing.js
@@ -1,14 +1,114 @@
 this.initialize = function() {
-	this.initialDownPoint = undefined;
-	this.drawingObject = undefined;
-	this.nameIndex = 1;
+	this.drawing_private = {};
+	this.future(0).clientWatch();
 };
+
+this.clientWatch = function() {
+	
+	var clients = this.find( "doc('http://vwf.example.com/clients.vwf')" )[0];
+
+	if ( clients !== undefined ) {
+		
+		clients.children.added = clients.events.add( function( index, child ) {
+			this.clientJoin( this.moniker );
+	    }, this );
+
+	    clients.children.removed = clients.events.add( function( index, child ) {
+			this.clientLeave( this.moniker );	
+	    }, this );
+
+	}
+
+};
+
+this.isValid = function( obj ) {
+    var objType = ( {} ).toString.call( obj ).match( /\s([a-zA-Z]+)/ )[ 1 ].toLowerCase();
+    return ( objType != 'null' && objType != 'undefined' );
+}; 
+
+this.clientJoin = function( moniker ) {
+
+	// mirrors the initial state of the toolbar
+	if ( !this.isValid( this.drawing_clients ) ) {
+		this.drawing_clients = {};
+	}
+	if ( this.drawing_clients[ moniker ] === undefined ) {
+		this.drawing_clients[ moniker ] = {  
+			"drawing_mode": 'none',
+			"drawing_visible": 'inherit',
+			"drawing_color": 'red',
+			"drawing_width": 4,
+			"drawing_parentPath": '//',
+			"drawing_opacity": 0.4,
+			"nameIndex": 1
+		};
+	}
+	this.drawing_clients = this.drawing_clients;
+
+};
+
+this.setUpPrivate = function( moniker ) {
+	
+	if ( this.drawing_private === undefined ) {
+		this.drawing_private = {};
+	}
+	if ( this.drawing_private[ moniker ] === undefined ) {
+		this.drawing_private[ moniker ] = {
+			"drawingObject": null,
+			"initialDownPoint": [ -1, -1 ],
+			"mouseDown": false
+		};	
+	}
+
+}
+
+this.clientLeave = function( moniker ) {
+	if ( this.drawing_clients[ moniker ] !== undefined ) {
+		delete this.drawing_clients[ moniker ];	
+		this.drawing_clients = this.drawing_clients;
+	}
+	if ( this.drawing_private[ moniker ] !== undefined ) {
+		delete this.drawing_private[ moniker ];	
+	}
+};
+
+this.setClientUIState = function( stateObj ) {
+
+	//console.info( "setClientUIState " + JSON.stringify( stateObj ) );
+	if ( stateObj !== undefined ) {
+		if ( !this.isValid( this.drawing_clients ) || !this.isValid( this.drawing_clients[ this.client ] ) ) {
+			this.clientJoin( this.client );
+		} 
+		var clients = this.drawing_clients;
+		var userState = clients[ this.client ];
+		for ( var property in stateObj ) {
+			userState[ property ] = stateObj[ property ];		
+		}
+		this.drawing_clients = clients;
+	}
+};
+
 
 this.pointerDown = function( eventData, nodeData ) {
 	
+	if ( !this.isValid( this.drawing_clients ) || !this.isValid( this.drawing_clients[ this.client ] ) ) {
+		this.clientJoin( this.client );
+	} 
+	if ( this.drawing_private === undefined || this.drawing_private[ this.client ] === undefined ) {
+		this.setUpPrivate( this.client );
+	}
+
+	var userState = this.drawing_clients[ this.client ];
+	var privateState = this.drawing_private[ this.client ];
+	var drawingMode = userState.drawing_mode;
+
+	if ( privateState.drawingObject !== null ) {
+		return;
+	}
+
 	var compExtends = undefined;
 
-	switch ( this.drawing_mode ) {
+	switch ( drawingMode ) {
 		
 		case "arc":
 		case "circle":
@@ -22,7 +122,7 @@ this.pointerDown = function( eventData, nodeData ) {
 		case "star":
 		case "text":
 		case "wedge":
-			compExtends	= "http://vwf.example.com/kinetic/"+this.drawing_mode+".vwf"; 
+			compExtends	= "http://vwf.example.com/kinetic/"+drawingMode+".vwf"; 
 			break;
 
 		case "freeDraw":
@@ -35,70 +135,92 @@ this.pointerDown = function( eventData, nodeData ) {
 	}
 
 	if ( compExtends !== undefined ) {
-		this.initialDownPoint = eventData.layer;
-		var parents = this.find( this.drawing_parentPath + "//shapes" );
+		privateState.initialDownPoint = eventData.layer;
+		var parents = this.find( userState.drawing_parentPath + "//shapes" );
 		var parent = parents.length > 0 ? parents[ 0 ] : this;
 		var shapeDef = {
 			"extends": compExtends,
 			"properties": {
 				"visible": false,
 				"position": eventData.layer,
-				"fill": this.drawing_color,
-				"opacity": this.drawing_opacity
+				"fill": userState.drawing_color,
+				"opacity": userState.drawing_opacity
 			}
 		};
 		var self = this;
-		var name = this.drawing_mode + this.nameIndex;
-		this.nameIndex++;
+		var selfMoniker = this.client;
+		var name = drawingMode + userState.nameIndex;
+		userState.nameIndex++;
 		parent.children.create( name, shapeDef, function( child ) {
-			self.drawingObject = child;
+			//console.info( "Child created name: " + child.name + "    " + child.id );
+			self.drawing_private[ selfMoniker ].drawingObject = child;
 		} ); 
 	}
 
 };
 
 this.pointerMove = function( eventData, nodeData ) {
+
+	if ( !this.isValid( this.drawing_clients ) || !this.isValid( this.drawing_clients[ this.client ] ) ) {
+		this.clientJoin( this.client );
+	} 
+	if ( this.drawing_private === undefined || this.drawing_private[ this.client ] === undefined ) {
+		this.setUpPrivate( this.client );
+	}
+
 	this.update( eventData, nodeData );
+
 };
 
 this.pointerUp = function( eventData, nodeData ) {
-	if ( this.drawingObject !== undefined  ) {
+
+	if ( this.drawing_private[ this.client ] !== undefined && this.drawing_private[ this.client ].drawingObject !== null  ) {
+		var drawingObject = this.drawing_private[ this.client ].drawingObject;
 		this.update( eventData, nodeData );
-		this.shapeCreated( this.drawingObject.id );
+		this.shapeCreated( drawingObject.id );
+		this.drawing_private[ this.client ].drawingObject = null;
 	}
-	this.drawingObject = undefined;
+
 }; 
 
 this.update = function( eventData, nodeData ) {
-	if ( this.drawingObject !== undefined ) {
-		if ( this.drawingObject.visible !== this.drawing_visible ) {
-			this.drawingObject.visible = this.drawing_visible;
+	
+	if ( this.drawing_private === undefined || this.drawing_private[ this.client ] === undefined || !this.isValid( this.drawing_clients ) ) {
+		return;
+	}
+
+	if ( this.drawing_private[ this.client ].drawingObject !== null ) {
+		
+		var userState = this.drawing_clients[ this.client ];		
+		var privateState = this.drawing_private[ this.client ];
+		var drawingObject = privateState.drawingObject;
+
+		if ( drawingObject.visible !== userState.drawing_visible ) {
+			drawingObject.visible = userState.drawing_visible;
 		}
-		var diffX = eventData.layer[ 0 ] - this.initialDownPoint[ 0 ];
-		var diffY = eventData.layer[ 1 ] - this.initialDownPoint[ 1 ];
-		var pos = [ eventData.layer[ 0 ], eventData.layer[ 1 ] ];
+		var diffX = eventData.layer[ 0 ] - privateState.initialDownPoint[ 0 ];
+		var diffY = eventData.layer[ 1 ] - privateState.initialDownPoint[ 1 ];
+		var pos = [ privateState.initialDownPoint[ 0 ], privateState.initialDownPoint[ 1 ] ];
 		var width = diffX;	
 		var height = diffY;
+		var radius;
 
-
-		console.info( "diffX = " + diffX + "     diffY = " + diffY );
-
-		switch ( this.drawing_mode ) {
+		switch ( userState.drawing_mode ) {
 			
 			case "arc":
-				var radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
-				this.drawingObject.angle = 30;
-				this.drawingObject.innerRadius = radius - this.drawing_width;
-				this.drawingObject.outerRadius = radius;
+				radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
+				drawingObject.angle = 30;
+				drawingObject.innerRadius = radius - this.drawing_width;
+				drawingObject.outerRadius = radius;
 				break;
 
 			case "ellipse":
 			case "circle":
-				this.drawingObject.radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
+				drawingObject.radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
 				break;
 
 			case "line":
-				this.drawingObject.points = [ this.initialDownPoint[ 0 ], this.initialDownPoint[ 1 ], eventData.layer[ 0 ], eventData.layer[ 1 ] ];
+				drawingObject.points = [ privateState.initialDownPoint[ 0 ], privateState.initialDownPoint[ 1 ], eventData.layer[ 0 ], eventData.layer[ 1 ] ];
 				break;
 
 			case "regularPolygon":
@@ -116,22 +238,22 @@ this.update = function( eventData, nodeData ) {
 					pos[ 1 ] += diffY;	
 					height = Math.abs( diffY );
 				} 
-				this.drawingObject.position = pos;
-				this.drawingObject.width = width;
-				this.drawingObject.height = height;
+				drawingObject.position = pos;
+				drawingObject.width = width;
+				drawingObject.height = height;
 				break;
 
 			case "ring":
-				var radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
-				this.drawingObject.innerRadius = radius - this.drawing_width;
-				this.drawingObject.outerRadius = radius;
+				radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
+				drawingObject.innerRadius = radius - userState.drawing_width;
+				drawingObject.outerRadius = radius;
 				break;
 
 			case "star":
-				var radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
-				this.drawingObject.points = 5;
-				this.drawingObject.innerRadius = radius * 60;
-				this.drawingObject.outerRadius = radius;
+				radius = Math.sqrt( ( diffX * diffX ) + ( diffY * diffY ) );
+				drawingObject.points = 5;
+				drawingObject.innerRadius = radius * 60;
+				drawingObject.outerRadius = radius;
 				break;
 
 			case "wedge":
