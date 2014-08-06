5c6e2e640094d5e42504c70ae0220f9770dd8e9f
Scott Haynes <scottnc27603@gmail.com>
2014-08-06 17:10:06 -0400

    Removes folders that were accidentally committed

--- public/sTDG/unit.js
+++
@@ -1,7 +0,0 @@
-this.addLocationOffset = function( offset ) {
-    this.location[ 0 ] += offset[ 0 ];
-    this.location[ 1 ] += offset[ 1 ];
-    this.location = this.location;
-}
-
-//@ sourceURL=unit.js
