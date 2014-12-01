e198bf684cd5f4da57271f65bc10b81035ef33ff
Scott Haynes <scottnc27603@gmail.com>
2014-12-01 17:38:28 -0500

    Updates the event name and parameters for the render

--- support/client/lib/vwf/model/mil-sym.js
+++ support/client/lib/vwf/model/mil-sym.js
@@ -464,11 +464,14 @@ define( [ "module",
         if ( node !== undefined && node.nodeType === "unit" && node.symbolID !== undefined ) {
             var iconRender = armyc2.c2sd.renderer.MilStdIconRenderer;
             var img = iconRender.Render( node.symbolID, node.modifiers );
+            
             var centerPt = img.getCenterPoint();
-            value = node.image = img.toDataUrl();
             var imgSize = img.getImageBounds();
-            self.kernel.fireEvent( node.ID, "imageChanged", [ node.image, imgSize.width, imgSize.height, centerPt.x, centerPt.y ] );
-            //self.kernel.fireEvent( node.ID, "imageChanged", [ node.image, imgSize.width, imgSize.height ] );
+            var symbolBounds = img.getSymbolBounds();
+
+            value = node.image = img.toDataUrl();
+
+            self.kernel.fireEvent( node.ID, "imageRendered", [ node.image, imgSize, centerPt, symbolBounds ] );
         } 
 
         return value;       
