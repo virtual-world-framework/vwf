6ed1d42389521b47ce1fbc2799bb58087084d220
Brett Swift <xclutch@cfl.rr.com>
2014-12-07 13:55:18 -0500

    Addressed PR comments

--- support/proxy/vwf.example.com/eventManager.js
+++
@@ -1,9 +0,0 @@
-this.addListener = function( eventName, callbackMethodName ) {
-    this.eventMap[ eventName ] = callbackMethodName;
-}
-
-this.removeListener = function( eventName ) {
-    delete this[ eventName ];
-}
-
-//@ sourceURL=http://vwf.example.com/eventManager.js
