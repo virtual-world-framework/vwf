faf48f588984e4216073d6f6879572d544cdcfc0
David Easter <this@davideaster.de>
2014-12-14 23:38:58 -0500

    External references for use by other parts of the documentation.

---
+++ support/client/lib/vwf/api/external.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Copyright 2014 United States Government, as represented by the Secretary of Defense, Under
+// Secretary of Defense (Personnel & Readiness).
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
+
+// References to external documents to be used by other parts of the documentation.
+
+
+/// @external Window
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Window}
+
+/// @function external:Window#requestAnimationFrame
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Window.requestAnimationFrame}
+
+
+/// @external WindowTimers
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers}
+
+/// @function external:WindowTimers#clearInterval
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers.clearInterval}
+
+/// @function external:WindowTimers#clearTimeout
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers.clearTimeout}
+
+/// @function external:WindowTimers#setInterval
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers.setInterval}
+
+/// @function external:WindowTimers#setTimeout
+/// @see {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers.setTimeout}
+
