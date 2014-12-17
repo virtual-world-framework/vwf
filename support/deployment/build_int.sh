a944843ded2435657eb8291f1287be6a27edbf1f
David Easter <this@davideaster.de>
2014-12-17 13:40:16 -0500

    Integration version of the bash build script.

---
+++ support/deployment/build_int.sh
@@ -0,0 +1,43 @@
+#!/bin/bash   
+
+
+if [ -d "/cygdrive/c/vwf" ]; then
+	rm -rf /cygdrive/c/vwf
+	rm -rf /cygdrive/c/vwfsource
+fi
+
+git clone --branch integration --recursive http://www.github.com/virtual-world-framework/vwf vwf
+
+git clone --branch integration --recursive http://www.github.com/virtual-world-framework/vwf vwfsource
+
+rm -rf /cygdrive/c/vwf/.git
+rm -rf /cygdrive/c/vwfsource/.git
+
+# Get Version File Ready For Windows Zip File Naming
+cat /cygdrive/c/vwf/support/client/lib/version.js | while read line;
+do
+if [[ "$line" == *return* ]]
+then
+temp1=${line#*[*};
+temp2=${temp1%*]*};
+temp2=${temp2//[[:space:]]};
+temp2=${temp2//,/.};
+echo $temp2 > /cygdrive/c/version.txt;
+fi;
+done
+
+
+
+
+
+cd vwf
+git submodule init
+git submodule update
+bundle install
+bundle exec rake
+
+mkdir node
+cd node
+wget http://nodejs.org/dist/latest/node.exe
+wget http://nodejs.org/dist/npm/npm-1.3.25.zip
+
