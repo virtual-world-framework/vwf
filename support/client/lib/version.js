ded7175f4285ef692571a0ff6b65bfe5c02a21a3
Allison Corey <allison.a.corey@lmco.com>
2015-02-20 10:06:47 -0500

    Update version and changelog for 0.7.0 release.

--- support/client/lib/version.js
+++ support/client/lib/version.js
@@ -30,7 +30,7 @@ define( function() {
     /// particuarly clever about it. Take care to keep the comment and formatting intact when
     /// bumping the version number.
 
-    var version = [ 0, 6, 25, "development", "" ];  // version-identifier
+    var version = [ 0, 7, 0, "development", "" ];  // version-identifier
 
     /// Render the version identifier as a SemVer-style string.
 
