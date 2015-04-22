f321e9eaec2716e7fd9f5096922f0abf8aa94329
Nathan Marshak <nathan.x.marshak@lmco.com>
2015-04-22 19:01:31 -0400

    Now the jPlayer driver doesn't set the URL (which may cause a load to trigger) if its being set to the same URL as before.

--- support/client/lib/vwf/model/jPlayer.js
+++ support/client/lib/vwf/model/jPlayer.js
@@ -452,29 +452,31 @@ define( [
         }
     }
 
-    Array.prototype.equals = function (array) {
-        // if the other array is a falsy value, return
-        if (!array)
-            return false;
-
-        // compare lengths - can save a lot of time 
-        if (this.length != array.length)
-            return false;
-
-        for (var i = 0, l=this.length; i < l; i++) {
-            // Check if we have nested arrays
-            if (this[i] instanceof Array && array[i] instanceof Array) {
-                // recurse into the nested arrays
-                if (!this[i].equals(array[i]))
-                    return false;       
-            }           
-            else if (this[i] != array[i]) { 
-                // Warning - two different object instances will never be equal: {x:20} != {x:20}
-                return false;   
-            }           
-        }       
-        return true;
-    }   
+/*
+ *    Array.prototype.equals = function (array) {
+ *        // if the other array is a falsy value, return
+ *        if (!array)
+ *            return false;
+ *
+ *        // compare lengths - can save a lot of time 
+ *        if (this.length != array.length)
+ *            return false;
+ *
+ *        for (var i = 0, l=this.length; i < l; i++) {
+ *            // Check if we have nested arrays
+ *            if (this[i] instanceof Array && array[i] instanceof Array) {
+ *                // recurse into the nested arrays
+ *                if (!this[i].equals(array[i]))
+ *                    return false;       
+ *            }           
+ *            else if (this[i] != array[i]) { 
+ *                // Warning - two different object instances will never be equal: {x:20} != {x:20}
+ *                return false;   
+ *            }           
+ *        }       
+ *        return true;
+ *    }   
+ */
 
     function setUrl( node, inputUrl ) {
 
@@ -487,10 +489,12 @@ define( [
             usingMultiUrls = false;
             url = inputUrl;
         }          
-        if( node.url && url && (node.url).equals( url ) ){
+        
+        if( node.url && url && ( node.url === url ) ){
             console.log("Setting redudant URL! Quitting!");
             return;
         }
+
         node.url = url;
 
         // If there is no jPlayerElement, there is nothing to do yet so we return.
