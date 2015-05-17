998adea9fb69092b3d09c94d785785c3d9934be0
Brett Swift <xclutch@cfl.rr.com>
2015-05-17 13:49:07 -0400

    Update shader material when defines are modified

--- support/client/lib/vwf/model/threejs.js
+++ support/client/lib/vwf/model/threejs.js
@@ -1657,6 +1657,7 @@ define( [ "module",
                                 defines[ names[ i ] ] = propertyValue[ names[ i ] ];
                             }
                             threeObject.defines = defines;
+                            threeObject.needsUpdate = true;
                         } else if ( propertyName === "vertexShader" ) {
                             value = propertyValue;
                             threeObject.vertexShader = value;
@@ -5598,6 +5599,13 @@ define( [ "module",
                     vectorArray.push( new THREE.Vector2( vector[0], vector[1] ) );
                 }
                 obj[ prop ].value = vectorArray;
+                break;
+            case 'iv1':
+                var intArray = [];
+                for ( var i = 0; i < value.length; i++ ) {
+                    intArray.push( Number( value[ i ] ) );
+                }
+                obj[ prop ].value = intArray;
                 break;
         } 
     
