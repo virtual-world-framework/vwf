96145cf47b6ce5dd370aa7c91070db9cae0f0566
youngca <christopher.a.young@lmco.com>
2017-06-22 11:24:20 -0400

    Update version to 2.6.6.

--- support/client/lib/ITDG/version.js
+++ support/client/lib/ITDG/version.js
@@ -26,7 +26,7 @@
 /// particuarly clever about it. Take care to keep the comment and formatting intact when
 /// bumping the version number.
 
-var version = [ 0, 8, 0, "", "", [ "ITDG", 2, 6, 5 ] ];  // version-identifier
+var version = [ 0, 8, 0, "", "", [ "ITDG", 2, 6, 6 ] ];  // version-identifier
 
 /// Render the version identifier as a SemVer-style string.
 
