6d1a3c4efa7963057629b622dda31c6fa1d6336e
David Easter <david.easter@lmco.com>
2018-01-10 12:41:47 -0500

    Add mock manifest locals

--- support/lobby/lib/locals.js
+++ support/lobby/lib/locals.js
@@ -149,3 +149,65 @@ export let session = {
     }
   }
 };
+
+export let manifest = {
+  "/index.vwf":
+    {},
+  "/test/component.vwf":
+    {},
+  "/test/index.vwf":
+    {},
+  "/test/json.vwf":
+    {},
+  "/test/yaml.vwf":
+    {},
+  "/ITDG/index.vwf": {
+    "One": {
+      "application":
+        "/ITDG/index.vwf",
+      "scenario": {
+        "state": {
+          "scenarioName": "One",
+          "scenarioTitle": "One"
+        },
+        "completion": {
+          "document": {
+            "instructors": 0,
+            "students": 0
+          }
+        },
+        "document": {
+          "uri": "/ITDG/B6eoGxgzNSHOCMgm/load/One/",
+          "timestamp": "2017-12-06T18:17:46.000Z"
+        }
+      },
+      "sessions": [
+        {
+          "state": {
+            "scenarioName":
+              "One",
+            "scenarioTitle":
+              "One",
+            "classroom": {
+              "company": "Co",
+              "platoon": "1",
+              "unit": "1"
+            },
+            "dateOfClass":
+              "2017-12-06T18:17:56.606Z"
+          },
+          "completion": {
+            "document": {
+              "instructors": 0,
+              "students": 0
+            }
+          },
+          "document": {
+            "uri": "/ITDG/1PTAXVPPpmIz7FQK/load/class_One_CoCo_Plt1_Unit1_2017/",
+            "timestamp": "2017-12-06T18:17:56.000Z"
+          }
+        }
+      ]
+    }
+  }
+};
