ab55e1ce998b9ca11dfd5dbf2ee506e641e546cb
David Easter <david.easter@lmco.com>
2018-01-10 23:45:13 -0500

    Configure the layout using version and user data

--- support/lobby/lib/Layout.js
+++ support/lobby/lib/Layout.js
@@ -4,9 +4,10 @@ import { TabContainer, Navbar, Image, Nav, NavItem, TabContent, TabPane, Button 
 import Scenarios from "./scenarios";
 import Sessions from "./sessions";
 import Review from "./review";
+import * as locals from "./locals";
 
 export default function Layout( props ) {
-  return <TabContainer generateChildId={ ( key, type ) => type + "-" + key }>
+  return <TabContainer defaultActiveKey={ locals.session.passport.user.instructor ? "scenarios" : "sessions" } generateChildId={ ( key, type ) => type + "-" + key }>
     <div>
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -16,28 +17,35 @@ export default function Layout( props ) {
             <a href="#">
               <span><Image className="logo_img_small" src="/ONR_Logo.jpg"/></span>
               <span>&ensp;</span>
-              <span>Title</span>
+              <span>{ locals.version.title }</span>
             </a>
           </Navbar.Brand>
         </Navbar.Header>
         <Navbar.Collapse id="navbar">
           <Nav>
-            <NavItem eventKey="scenarios">Scenarios</NavItem>
-            <NavItem eventKey="sessions">Sessions</NavItem>
-            <NavItem eventKey="review">Review</NavItem>
+            { locals.session.passport.user.instructor && <NavItem eventKey="scenarios">Scenarios</NavItem> }
+            { locals.session.passport.user.instructor && <NavItem eventKey="sessions">Sessions</NavItem> }
+            { locals.session.passport.user.instructor && <NavItem eventKey="review">Review</NavItem> }
           </Nav>
           <Navbar.Form pullRight method="post" action="/logout">
             <Button type="submit" bsStyle="link"> Logout </Button>
           </Navbar.Form>
           <Navbar.Text pullRight>
-            Last, First MI.
+            {
+              ( locals.session.passport.user.last_name || "" ) + ( locals.session.passport.user.last_name && ( locals.session.passport.user.first_name || locals.session.passport.user.middle_initial ) ? ", " : "" )  +
+              ( locals.session.passport.user.first_name || "" ) + ( locals.session.passport.user.first_name && locals.session.passport.user.middle_initial ? " " : "" )  +
+              ( locals.session.passport.user.middle_initial || "" ) + ( locals.session.passport.user.middle_initial ? "." : "" )
+            }
           </Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
       <TabContent animation={ false }>
-        <TabPane eventKey="scenarios"><Scenarios/></TabPane>
-        <TabPane eventKey="sessions"><Sessions/></TabPane>
-        <TabPane eventKey="review"><Review/></TabPane>
+        { locals.session.passport.user.instructor &&
+          <TabPane eventKey="scenarios"><Scenarios/></TabPane> }
+        { true &&
+          <TabPane eventKey="sessions"><Sessions/></TabPane> }
+        { locals.session.passport.user.instructor &&
+          <TabPane eventKey="review"><Review/></TabPane> }
       </TabContent>
     </div>
   </TabContainer>;
