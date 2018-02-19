5aeb62a44185607f61040bae34c84a7db72dac3d
David Easter <david.easter@lmco.com>
2018-02-19 02:08:32 -0500

    Review as `react-table`

--- support/lobby/lib/Review.js
+++ support/lobby/lib/Review.js
@@ -1,59 +1,57 @@
 import React from "react";
 import { Table, Button } from "react-bootstrap";
+import ReactTable from "react-table";
 import dateFormat from "dateformat";
 
 export default function Reviews( props ) {
-  return <Table striped>
-    <thead>
-      <Head/>
-    </thead>
-    <tbody>
-      <ReviewRows records={ props.records }/>
-    </tbody>
-  </Table>;
+  return <ReactTable data={ props.records } columns={ columns } className="-striped"/>;
 }
 
-function Head( props ) {
-  return <tr>
-    <th className="col-sm-5">
-      Scenario
-    </th><th className="col-sm-1">
-      Company
-    </th><th className="col-sm-1">
-      Platoon
-    </th><th className="col-sm-1">
-      Unit
-    </th><th className="col-sm-3">
-      Date
-    </th><th className="col-sm-1">
-      &nbsp;
-    </th>
-  </tr>;
+const columns = [ {
+  Header:
+    "Scenario",
+  accessor:
+    "session.state.scenarioTitle",
+}, {
+  Header:
+    "Company",
+  accessor:
+    "session.state.classroom.company",
+}, {
+  Header:
+    "Platoon",
+  accessor:
+    "session.state.classroom.platoon",
+}, {
+  Header:
+    "Unit",
+  accessor:
+    "session.state.classroom.unit",
+}, {
+  Header:
+    "Date",
+  accessor:
+    "session.document.timestamp",
+  Cell:
+    function Cell( props ) { return <DateCell { ...props }/> },
+}, {
+  Header:
+    "",
+  accessor:
+    "session",
+  Cell:
+    function Cell( props ) { return <ActionCell { ...props }/> },
+} ];
+
+class DateCell extends React.Component {
+  render() {
+    return dateFormat( this.props.value );
+  }
 }
 
-function ReviewRows( props ) {
-  return <React.Fragment>
-    { props.records.map( ( record, index ) => <Review key={ index } { ...record }/> ) }
-  </React.Fragment>;
-}
-
-function Review( props ) {
-  const scenario = props.scenario,
-    session = props.session;
-  return <tr>
-    <td>
-      { session.state.scenarioTitle }
-    </td><td>
-      { session.state.classroom.company }
-    </td><td>
-      { session.state.classroom.platoon }
-    </td><td>
-      { session.state.classroom.unit }
-    </td><td>
-      { dateFormat( session.document.timestamp ) }
-    </td><td>
-      <Button href={ session.instance || session.document.uri } target="_blank"
-        bsSize="small" bsStyle="link"> Review </Button>
-    </td>
-  </tr>;
+class ActionCell extends React.Component {
+  render() {
+    return <Button href={ this.props.value.instance || this.props.value.document.uri } target="_blank"
+      bsSize="small" bsStyle="link"> Review </Button>;
+  }
 }
