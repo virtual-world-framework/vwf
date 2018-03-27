44836f0e70319f6c1122b9e8e904938e9d7a2b95
youngca <christopher.a.young@lmco.com>
2018-03-27 16:07:57 -0400

    Add route and routeSequenceNumber as special modifiers to aviation control points.

--- support/client/lib/mil-sym/cws.js
+++ support/client/lib/mil-sym/cws.js
@@ -4815,17 +4815,17 @@ define( function(){
                   "TACGRP.C2GM.AVN.PNT.ACP": {
                       "symbolID": "GUGPAPP---****X",
                       "tag": "TACGRP",
-                      "specialModifiers": [ "altitudeDepth" ]
+                      "specialModifiers": [ "altitudeDepth", "route", "routeSequenceNum" ]
                   },
                   "TACGRP.C2GM.AVN.PNT.COMMCP": {
                       "symbolID": "GUGPAPC---****X",
                       "tag": "TACGRP",
-                      "specialModifiers": [ "altitudeDepth" ]
+                      "specialModifiers": [ "altitudeDepth", "route", "routeSequenceNum" ]
                   },
                   "TACGRP.C2GM.AVN.PNT.PUP": {
                       "symbolID": "GUGPAPU---****X",
                       "tag": "TACGRP",
-                      "specialModifiers": [ "altitudeDepth" ]
+                      "specialModifiers": [ "altitudeDepth", "route", "routeSequenceNum" ]
                   },
                   "TACGRP.C2GM.AVN.PNT.DAPP": {
                       "symbolID": "GUGPAPD---****X",
