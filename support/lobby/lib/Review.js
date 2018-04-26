a425627f056da6882a6dbde6a658876ed575a24d
Eric Burns <eric.burns@lmco.com>
2018-04-26 13:04:04 -0400

    Line up text and links on lobby Review tab rows

--- support/lobby/lib/Review.js
+++ support/lobby/lib/Review.js
@@ -17,28 +17,36 @@ const columns = [ {
     "Scenario",
   accessor:
     "session.state.scenarioTitle",
+  Cell:
+    props => <TextCell { ...props }/>,
 }, {
   Header:
     "Company",
   accessor:
     "session.state.classroom.company",
+  Cell:
+    props => <TextCell { ...props }/>,
 }, {
   Header:
     "Platoon",
   accessor:
     "session.state.classroom.platoon",
+  Cell:
+    props => <TextCell { ...props }/>,
 }, {
   Header:
     "Unit",
   accessor:
     "session.state.classroom.unit",
+  Cell:
+    props => <TextCell { ...props }/>,
 }, {
   Header:
     "Date",
   accessor:
     "session.document.timestamp",
   Cell:
-    function Cell( props ) { return <DateCell { ...props }/> },
+    props => <DateCell { ...props }/>,
   filterable:
     false,
   width:
@@ -51,7 +59,7 @@ const columns = [ {
   accessor:
     "session",
   Cell:
-    function Cell( props ) { return <ReviewCell { ...props }/> },
+    props => <ReviewCell { ...props }/>,
   sortable:
     false,
   filterable:
@@ -64,16 +72,31 @@ const columns = [ {
   accessor:
     "session",
   Cell:
-    function Cell( props ) { return <ResumeCell { ...props }/> },
+    props => <ResumeCell { ...props }/>,
   sortable:
     false,
   filterable:
     false,
 } ];
 
+const buttonContainerStyle = {
+  width: "100%",
+  textAlign: "center"
+}
+
+class TextCell extends React.Component {
+  render() {
+    return (
+      <div style={{ height: "100%", display: "flex", alignItems: "center" }}>
+        { this.props.value }
+      </div>
+    );
+  }
+}
+
 class DateCell extends React.Component {
   render() {
-    return dateFormat( this.props.value );
+    return <TextCell value={ dateFormat( this.props.value ) } />;
   }
 }
 
@@ -81,7 +104,7 @@ class ReviewCell extends React.Component {
   render() {
     const url = ( this.props.value.instance || this.props.value.document.uri ) + "?isReview=true";
     return (
-      <div style={{ width: "100%", textAlign: "center"}}>
+      <div style={ buttonContainerStyle }>
         <Button href={ url } target="_blank" bsSize="small" bsStyle="link">
           Review
         </Button>
@@ -93,7 +116,7 @@ class ReviewCell extends React.Component {
 class ResumeCell extends React.Component {
   render() {
     return (
-      <div style={{ width: "100%", textAlign: "center"}}>
+      <div style={ buttonContainerStyle }>
         <Button
           href={ this.props.value.instance || this.props.value.document.uri }
           target="_blank"
