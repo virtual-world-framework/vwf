320e76c6c2713efef9f0d561eeda3ed9c2bf8ced
youngca <christopher.a.young@lmco.com>
2018-05-17 13:07:05 -0400

    Update to ITDG version 3.2.1.

--- support/client/lib/version.js
+++ support/client/lib/version.js
@@ -30,7 +30,7 @@ define( function() {
     /// particuarly clever about it. Take care to keep the comment and formatting intact when
     /// bumping the version number.
 
-    var version = [ 0, 8, 0, "", "", [ "ITDG", 3, 2, 0 ] ];  // version-identifier
+    var version = [ 0, 8, 0, "", "", [ "ITDG", 3, 2, 1 ] ];  // version-identifier
 
     /// Render the version identifier as a SemVer-style string.
 
